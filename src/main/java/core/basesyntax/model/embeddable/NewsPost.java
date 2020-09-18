8ef8cdb39dee79aba4953b53cd6bfc93b33b3bdb
dmytro-hryhoruk <dimamyhgrygoruk@gmail.com>
2020-09-18 11:38:06 +0300

    added solution for embedded value

--- src/main/java/core/basesyntax/model/embeddable/NewsPost.java
+++ src/main/java/core/basesyntax/model/embeddable/NewsPost.java
@@ -1,12 +1,31 @@
 package core.basesyntax.model.embeddable;
 
 import java.time.LocalDate;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "news_post")
 public class NewsPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private LocalDate postDate;
     private String topic;
     private String content;
+    @Embedded
     private PostMetadata metadata;
+
+    public NewsPost() {
+    }
+
+    public NewsPost(PostMetadata metadata) {
+        this.metadata = metadata;
+    }
 
     public LocalDate getPostDate() {
         return postDate;
--- src/main/java/core/basesyntax/model/embeddable/PostMetadata.java
+++ src/main/java/core/basesyntax/model/embeddable/PostMetadata.java
@@ -1,8 +1,25 @@
 package core.basesyntax.model.embeddable;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@AttributeOverrides({
+        @AttributeOverride(name = "size", column = @Column(name = "metadata_size")),
+        @AttributeOverride(name = "hashSum", column = @Column(name = "metadata_hash_sum"))
+})
 public class PostMetadata {
     private long size; // in bytes
     private String hashSum;
+
+    public PostMetadata() {
+    }
+
+    public PostMetadata(long size) {
+        this.size = size;
+    }
 
     public long getSize() {
         return size;
