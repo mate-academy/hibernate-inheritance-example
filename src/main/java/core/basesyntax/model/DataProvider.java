e4a06b18fc9eff5d633e165a65c492be4cab0a1c
dmytro-hryhoruk <dimamyhgrygoruk@gmail.com>
2020-09-18 11:43:19 +0300

    added DataProvider

---
+++ src/main/java/core/basesyntax/model/DataProvider.java
@@ -0,0 +1,80 @@
+package core.basesyntax.model;
+
+import java.util.List;
+import core.basesyntax.model.embeddable.NewsPost;
+import core.basesyntax.model.embeddable.PostMetadata;
+import core.basesyntax.model.figure.Circle;
+import core.basesyntax.model.figure.Triangle;
+import core.basesyntax.model.ma.Coach;
+import core.basesyntax.model.ma.Mentor;
+import core.basesyntax.model.machine.Car;
+import core.basesyntax.model.machine.Machine;
+import core.basesyntax.model.machine.Truck;
+import core.basesyntax.model.zoo.Animal;
+import core.basesyntax.model.zoo.Cat;
+import core.basesyntax.model.zoo.Dog;
+
+public class DataProvider {
+    public static List<NewsPost> getPosts() {
+        return List.of(new NewsPost(new PostMetadata(20000)),
+                new NewsPost(new PostMetadata(10000)),
+                new NewsPost(new PostMetadata(5000)),
+                new NewsPost(new PostMetadata(1000)),
+                new NewsPost(new PostMetadata(15000)));
+    }
+
+    public static List<Circle> getCircles() {
+        return List.of(new Circle("red"),
+                new Circle("black"),
+                new Circle("green"));
+    }
+
+    public static List<Triangle> getTriangles() {
+        return List.of(new Triangle("yellow"),
+                new Triangle("red"),
+                new Triangle("red"));
+    }
+
+    public static List<Coach> getCoaches() {
+        return List.of(new Coach(6),
+                new Coach(3),
+                new Coach(4),
+                new Coach(10),
+                new Coach(8));
+    }
+
+    public static List<Mentor> getMentors() {
+        return List.of(new Mentor(18),
+                new Mentor(20),
+                new Mentor(21),
+                new Mentor(19),
+                new Mentor(23));
+    }
+
+    public static List<Machine> getMachines() {
+        return List.of(new Car(2015),
+                new Car(2020),
+                new Car(2010),
+                new Car(2012),
+                new Car(2009),
+                new Truck(2015),
+                new Truck(2016),
+                new Truck(2021),
+                new Truck(2010),
+                new Truck(2012));
+    }
+
+    public static List<Animal> getAnimals() {
+        return List.of(new Cat("Adam"),
+                new Cat("Brad"),
+                new Cat("Avn"),
+                new Cat("Merlin"),
+                new Cat("Vovan"),
+                new Dog("April"),
+                new Dog("Brad"),
+                new Dog("Adamant"),
+                new Dog("Merlin"),
+                new Dog("Vovan"));
+    }
+
+}
--- src/main/java/core/basesyntax/model/Main.java
+++ src/main/java/core/basesyntax/model/Main.java
@@ -1,100 +1,68 @@
 package core.basesyntax.model;
 
-import java.util.List;
+import core.basesyntax.model.embeddable.NewsPost;
+import core.basesyntax.model.embeddable.dao.NewsPostDao;
+import core.basesyntax.model.embeddable.dao.NewsPostDaoImpl;
+import core.basesyntax.model.figure.Circle;
+import core.basesyntax.model.figure.Triangle;
+import core.basesyntax.model.figure.dao.CircleDao;
+import core.basesyntax.model.figure.dao.TriangleDao;
+import core.basesyntax.model.figure.dao.impl.CircleDaoImpl;
+import core.basesyntax.model.figure.dao.impl.TriangleDaoImpl;
 import core.basesyntax.model.ma.Coach;
 import core.basesyntax.model.ma.Mentor;
 import core.basesyntax.model.ma.dao.CoachDao;
 import core.basesyntax.model.ma.dao.MentorDao;
 import core.basesyntax.model.ma.dao.impl.CoachDaoImpl;
 import core.basesyntax.model.ma.dao.impl.MentorDaoImpl;
-import core.basesyntax.model.machine.Car;
 import core.basesyntax.model.machine.Machine;
-import core.basesyntax.model.machine.Truck;
 import core.basesyntax.model.machine.dao.MachineDao;
 import core.basesyntax.model.machine.dao.impl.MachineDaoImpl;
-import core.basesyntax.model.zoo.Cat;
-import core.basesyntax.model.zoo.Dog;
-import core.basesyntax.model.zoo.dao.CatDao;
-import core.basesyntax.model.zoo.dao.DogDao;
-import core.basesyntax.model.zoo.dao.impl.CatDaoImpl;
-import core.basesyntax.model.zoo.dao.impl.DogDaoImpl;
+import core.basesyntax.model.zoo.Animal;
+import core.basesyntax.model.zoo.dao.AnimalDao;
+import core.basesyntax.model.zoo.dao.impl.AnimalDaoImpl;
 
 public class Main {
     public static void main(String[] args) {
-        CatDao catDao = new CatDaoImpl();
-        for (Cat cat : getCats()) {
-            catDao.save(cat);
+        AnimalDao animalDao = new AnimalDaoImpl();
+        for (Animal animal : DataProvider.getAnimals()) {
+            animalDao.save(animal);
         }
-        DogDao dogDao = new DogDaoImpl();
-        for (Dog dog : getDogs()) {
-            dogDao.save(dog);
-        }
-        catDao.findByFirstLetter('A').forEach(System.out::println);
-        dogDao.findByFirstLetter('A').forEach(System.out::println);
-
+        animalDao.findByFirstLetter('A').forEach(System.out::println);
         MachineDao machineDao = new MachineDaoImpl();
-        for (Machine machine : getMachines()) {
+        for (Machine machine : DataProvider.getMachines()) {
             machineDao.save(machine);
         }
         machineDao.getOlderThan(4).forEach(System.out::println);
 
         CoachDao coachDao = new CoachDaoImpl();
-        for (Coach coach : getCoaches()) {
+        for (Coach coach : DataProvider.getCoaches()) {
             coachDao.save(coach);
         }
         MentorDao mentorDao = new MentorDaoImpl();
-        for (Mentor mentor : getMentors()) {
+        for (Mentor mentor : DataProvider.getMentors()) {
             mentorDao.save(mentor);
         }
         coachDao.getWithExperienceMoreThan(5).forEach(System.out::println);
         mentorDao.getOlderThan(20).forEach(System.out::println);
-    }
 
+        CircleDao circleDao = new CircleDaoImpl();
+        for (Circle circle : DataProvider.getCircles()) {
+            circleDao.save(circle);
+        }
 
-    private static List<Coach> getCoaches() {
-        return List.of(new Coach(6),
-                new Coach(3),
-                new Coach(4),
-                new Coach(10),
-                new Coach(8));
-    }
+        TriangleDao triangleDao = new TriangleDaoImpl();
+        for (Triangle triangle : DataProvider.getTriangles()) {
+            triangleDao.save(triangle);
+        }
+        circleDao.getByColor("red").forEach(System.out::println);
+        triangleDao.getByColor("red").forEach(System.out::println);
 
-    private static List<Mentor> getMentors() {
-        return List.of(new Mentor(18),
-                new Mentor(20),
-                new Mentor(21),
-                new Mentor(19),
-                new Mentor(23));
-    }
-
-    private static List<Machine> getMachines() {
-        return List.of(new Car(2015),
-                new Car(2020),
-                new Car(2010),
-                new Car(2012),
-                new Car(2009),
-                new Truck(2015),
-                new Truck(2016),
-                new Truck(2021),
-                new Truck(2010),
-                new Truck(2012));
-    }
-
-    private static List<Cat> getCats() {
-        return List.of(new Cat("Adam"),
-                new Cat("Brad"),
-                new Cat("Avn"),
-                new Cat("Merlin"),
-                new Cat("Vovan"));
-    }
-
-
-    private static List<Dog> getDogs() {
-        return List.of(new Dog("April"),
-                new Dog("Brad"),
-                new Dog("Adamant"),
-                new Dog("Merlin"),
-                new Dog("Vovan"));
+        NewsPostDao postDao = new NewsPostDaoImpl();
+        for (NewsPost post : DataProvider.getPosts()) {
+            postDao.save(post);
+        }
+        postDao.getWithMetadataLargerThan(10000);
     }
 
 }
