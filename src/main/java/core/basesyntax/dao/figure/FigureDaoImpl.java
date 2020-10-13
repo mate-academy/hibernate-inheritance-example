7622a2a3c5aeacb500d2ef6c035512496231c048
Leonid <rommel@ukr.net>
2020-10-13 16:24:05 +0300

    figure started

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -2,21 +2,23 @@ package core.basesyntax.dao.figure;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
+import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
+public class FigureDaoImpl<T extends Figure> extends AbstractDao<Figure> implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
-    public T save(T figure) {
-        return null;
-    }
-
-    @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()){
+            return session.createQuery("from " + clazz.getSimpleName()
+                                       + " c where c.color = :color", clazz)
+                    .setParameter("color", color)
+                    .getResultList();
+        }
     }
 }
