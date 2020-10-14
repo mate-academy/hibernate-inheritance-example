4d96f4b16114f98a7b543094fdf18e3fe95792b7
Laszlo Petechuk <frostpv@gmail.com>
2020-10-14 18:56:58 +0300

    mvn clean

--- src/test/java/core/basesyntax/dao/machine/MachineDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/machine/MachineDaoImplTest.java
@@ -8,8 +8,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.sound.midi.Track;
-
 public class MachineDaoImplTest extends AbstractTest {
     private MachineDao machineDao;
 
