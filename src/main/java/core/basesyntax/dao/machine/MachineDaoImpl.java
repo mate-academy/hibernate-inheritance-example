008a42be13963c049b15cf00ceacc0b46feccd75
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-10-15 00:06:13 +0300

    added solution

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -1,9 +1,14 @@
 package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDate;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     protected MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,36 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save Machine " + machine.getMaker(), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> query = session.createQuery("FROM Machine "
+                    + "WHERE :year - year > :param",Machine.class);
+            query.setParameter("param", age);
+            query.setParameter("year", LocalDate.now().getYear());
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get Machine with older then " + age, e);
+        }
     }
 }
