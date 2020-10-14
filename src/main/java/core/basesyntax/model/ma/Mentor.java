b93e6ad44d8778128b25f9bdc7e9c08970032c3f
Laszlo Petechuk <frostpv@gmail.com>
2020-10-14 15:30:40 +0300

    add Entity Mentor and Coach.

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,12 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mentor")
+@PrimaryKeyJoinColumn(name = "id")
 public class Mentor extends Person {
 
 }
