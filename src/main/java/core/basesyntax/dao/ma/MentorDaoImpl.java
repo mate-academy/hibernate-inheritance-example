13da3bf0748807c2504ab1695f03041fecb0247c
Laszlo Petechuk <frostpv@gmail.com>
2020-10-14 18:10:13 +0300

    add test for MentorDaoImpl

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,15 +2,23 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
+
     public MentorDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Mentor> userQuery =
+                    session.createQuery("from Mentor where age > :age", Mentor.class);
+            userQuery.setParameter("age", age);
+            return userQuery.getResultList();
+        }
     }
 }
--- src/test/java/core/basesyntax/dao/ma/CoachDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/ma/CoachDaoImplTest.java
@@ -1,5 +1,26 @@
 package core.basesyntax.dao.ma;
 
-public class CoachDaoImplTest {
+import core.basesyntax.dao.AbstractTest;
+import core.basesyntax.model.ma.Coach;
+import core.basesyntax.model.ma.Mentor;
+import core.basesyntax.model.ma.Person;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class CoachDaoImplTest extends AbstractTest {
+    private CoachDao coachDao;
+    @Override
+    protected Class<?>[] entities() {
+        return new Class[] {
+                Person.class,
+                Coach.class,
+                Mentor.class
+        };
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        CoachDao coachDao = new CoachDaoImpl(getSessionFactory());
+    }
 }
