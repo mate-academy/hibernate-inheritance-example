418aa7e35cc7015f7690a3fd3c820b4e78f45dbe
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-10-14 21:12:35 +0300

    animalDao implemented

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,11 +3,14 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
-
 import core.basesyntax.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -20,13 +23,32 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
         Session session = null;
         try{
             session = HibernateUtil.getSessionFactory().openSession();
-
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert animal" + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
-        return null;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Animal> criteriaQuery = criteriaBuilder.createQuery(Animal.class);
+            Root<Animal> root = criteriaQuery.from(Animal.class);
+            criteriaQuery.select(root)
+                    .where(criteriaBuilder.like(root.get("name"), Character.toString(character) + "%"));
+            Query<Animal> query = session.createQuery(criteriaQuery);
+            return query.getResultList();
+        }
     }
 }
