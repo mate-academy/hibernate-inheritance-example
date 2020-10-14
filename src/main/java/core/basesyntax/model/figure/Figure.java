37ac52dccb82c0f15f256be88c29ddecac0b23a4
Laszlo Petechuk <frostpv@gmail.com>
2020-10-14 16:09:06 +0300

    Add figures entity annotations.

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,7 +1,23 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.*;
+
+@Entity
+@MappedSuperclass
+@Table(name = "figure")
 public class Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getColor() {
         return color;
