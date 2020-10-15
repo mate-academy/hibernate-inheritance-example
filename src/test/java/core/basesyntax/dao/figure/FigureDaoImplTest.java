fb056a53511a73d2a8a7971389979b7b0bff0687
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-10-15 14:01:28 +0300

    added fix daoIml logic, and code styl, rollback uncomplete tests2

--- src/test/java/core/basesyntax/dao/figure/FigureDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/figure/FigureDaoImplTest.java
@@ -1,36 +1,5 @@
 package core.basesyntax.dao.figure;
 
-import core.basesyntax.dao.AbstractTest;
-import core.basesyntax.model.figure.Circle;
-import core.basesyntax.model.figure.Figure;
-import core.basesyntax.model.figure.Triangle;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+public class FigureDaoImplTest {
 
-public class FigureDaoImplTest extends AbstractTest {
-    private FigureDao<Figure> figureDao;
-    private Circle circle;
-    private Triangle triangle;
-
-    @Override
-    protected Class<?>[] entities() {
-        return new Class[]{
-                Circle.class,
-                Figure.class,
-                Triangle.class
-        };
-    }
-
-    @Before
-    public void setUp() {
-        figureDao = new FigureDaoImpl(getSessionFactory());
-        circle = new Circle();
-        circle.setColor("red");
-        circle.setRadius(15);
-
-        triangle = new Triangle();
-        triangle.setColor("red");
-        triangle.setArea(36);
-    }
 }
