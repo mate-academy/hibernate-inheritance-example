019523e5db53308fe0362af492ca8b8166d41848
Borisko130 <krimenyukb@gmail.com>
2020-10-15 17:27:18 +0300

    Corrected naming for id columns of Coach and Mentor

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
-@PrimaryKeyJoinColumn(name = "mentor_Id")
+@PrimaryKeyJoinColumn(name = "mentor_id")
 public class Mentor extends Person {
 
 }
