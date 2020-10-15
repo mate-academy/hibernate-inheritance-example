a5edc0dca24782b816b59a14972f648561f1d193
Alexey <gradiviro@gmail.com>
2020-10-15 12:24:44 +0300

    added inheritance, ready for review

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +13,14 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Coach> query = session.createQuery("FROM Coach"
+                    + " WHERE experience >=: years", Coach.class);
+            query.setParameter("years", years);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get coaches with experience greater then "
+                    + years, e);
+        }
     }
 }
