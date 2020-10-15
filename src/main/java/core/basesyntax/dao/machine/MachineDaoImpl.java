27ab9d8a9945a8c8217fcca5fc97a5242fef5f6a
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-10-15 13:17:34 +0300

    import order changed, unused imports removed

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,10 +2,8 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
-
 import java.time.LocalDateTime;
 import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
