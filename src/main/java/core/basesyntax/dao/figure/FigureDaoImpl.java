13d08d7de728148663725845adfa187f8a8f1464
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-10-15 09:56:47 +0300

    added AnimalDaoImpl and FigureDaoImpl

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,20 +3,27 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
-public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
+public class FigureDaoImpl<T extends Figure> extends AbstractDao<T> implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public T save(T figure) {
-        return null;
+        return super.save(figure);
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> userQuery = session.createQuery("FROM "
+                    + clazz.getSimpleName() + " WHERE color LIKE :color", clazz);
+            userQuery.setParameter("color", color);
+            return userQuery.getResultList();
+        }
     }
 }
