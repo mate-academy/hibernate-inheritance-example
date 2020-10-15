84ef7e8922506d0e266b5dec84f8b4357c6162d0
Martynice <marticiy@ukr.net>
2020-10-15 11:59:31 +0300

    removed some annotations

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,10 +1,8 @@
 package core.basesyntax.model.ma;
 
 import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
-@PrimaryKeyJoinColumn(name = "mentor_id")
 public class Mentor extends Person {
 
 }
