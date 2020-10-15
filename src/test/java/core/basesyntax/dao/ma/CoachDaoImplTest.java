d3966b7310f3b5a47890cac30f5af2c852edde36
Borisko130 <krimenyukb@gmail.com>
2020-10-15 12:31:30 +0300

    Implemented MentorDaoImplTest and CoachDaoImplTest

--- src/test/java/core/basesyntax/dao/ma/CoachDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/ma/CoachDaoImplTest.java
@@ -1,5 +1,57 @@
 package core.basesyntax.dao.ma;
 
-public class CoachDaoImplTest {
+import core.basesyntax.dao.AbstractTest;
+import core.basesyntax.model.ma.Coach;
+import core.basesyntax.model.ma.Mentor;
+import core.basesyntax.model.ma.Person;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CoachDaoImplTest extends AbstractTest {
+    private CoachDao coachDao;
+    @Override
+    protected Class<?>[] entities() {
+        return new Class[] {
+                Person.class,
+                Coach.class,
+                Mentor.class
+        };
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        coachDao = new CoachDaoImpl(getSessionFactory());
+        Coach coach1 = new Coach();
+        coach1.setExperience(7);
+        coach1.setName("Alice");
+        coachDao.save(coach1);
+        Coach coach2 = new Coach();
+        coach2.setExperience(2);
+        coach2.setName("Bob");
+        coachDao.save(coach2);
+        Coach coach3 = new Coach();
+        coach3.setExperience(1);
+        coach3.setName("Charlie");
+        coachDao.save(coach3);
+    }
+
+    @Test
+    public void coachFindByExperienceAll_Ok() {
+        List<Coach> actual = coachDao.findByExperienceGreaterThan(0);
+        Assert.assertNotNull(actual);
+        Assert.assertNotNull(actual.get(0).getId());
+        Assert.assertEquals(3, actual.size());
+    }
+
+    @Test
+    public void coachFindByMoreThan5_Ok() {
+        List<Coach> actual = coachDao.findByExperienceGreaterThan(5);
+        Assert.assertNotNull(actual);
+        Assert.assertNotNull(actual.get(0).getId());
+        Assert.assertEquals(1, actual.size());
+    }
 
 }
--- src/test/java/core/basesyntax/dao/ma/MentorDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/ma/MentorDaoImplTest.java
@@ -1,5 +1,57 @@
 package core.basesyntax.dao.ma;
 
-public class MentorDaoImplTest {
+import core.basesyntax.dao.AbstractTest;
+import core.basesyntax.model.ma.Coach;
+import core.basesyntax.model.ma.Mentor;
+import core.basesyntax.model.ma.Person;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.List;
+
+public class MentorDaoImplTest extends AbstractTest {
+    private MentorDao mentorDao;
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class[] {
+                Person.class,
+                Coach.class,
+                Mentor.class
+        };
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        mentorDao = new MentorDaoImpl(getSessionFactory());
+        Mentor mentor1 = new Mentor();
+        mentor1.setAge(21);
+        mentor1.setName("Alice");
+        mentorDao.save(mentor1);
+        Mentor mentor2 = new Mentor();
+        mentor2.setAge(31);
+        mentor2.setName("Bob");
+        mentorDao.save(mentor2);
+        Mentor mentor3 = new Mentor();
+        mentor3.setAge(15);
+        mentor3.setName("Charlie");
+        mentorDao.save(mentor3);
+    }
+
+    @Test
+    public void mentorFindByAgeAll_Ok() {
+        List<Mentor> actual = mentorDao.findByAgeGreaterThan(14);
+        Assert.assertNotNull(actual);
+        Assert.assertNotNull(actual.get(0).getId());
+        Assert.assertEquals(3, actual.size());
+    }
+
+    @Test
+    public void mentorFindByAgeOlder20_Ok() {
+        List<Mentor> actual = mentorDao.findByAgeGreaterThan(20);
+        Assert.assertNotNull(actual);
+        Assert.assertNotNull(actual.get(0).getId());
+        Assert.assertEquals(2, actual.size());
+    }
 }
