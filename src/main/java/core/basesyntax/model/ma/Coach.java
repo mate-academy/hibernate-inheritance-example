81fbd8b3505cbfa64c3aa29bb569e93a4c3725b9
Laszlo Petechuk <frostpv@gmail.com>
2020-10-15 12:45:40 +0300

    deleted annotations

--- src/main/java/core/basesyntax/model/ma/Coach.java
+++ src/main/java/core/basesyntax/model/ma/Coach.java
@@ -1,11 +1,9 @@
 package core.basesyntax.model.ma;
 
 import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
-@PrimaryKeyJoinColumn(name = "id")
 @Table (name = "coach")
 public class Coach extends Person {
     public enum Track {
--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,12 +1,10 @@
 package core.basesyntax.model.ma;
 
 import javax.persistence.Entity;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "mentor")
-@PrimaryKeyJoinColumn(name = "id")
 public class Mentor extends Person {
 
 }
