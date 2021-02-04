1f49bfceab9114cff7e47bc255b7f34c60ba3c79
engineer027 <dp270597dma@gmail.com>
2021-02-04 16:21:11 +0200

    Add the implementation of Single table inheritance strategy Implement all AnimalDaoImpl methods

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -2,8 +2,19 @@ package core.basesyntax.dao.animal;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
+
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +23,39 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert figure entity "
+                    + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> query = session
+                    .createQuery("SELECT a FROM Animal a "
+                                    + "WHERE a.name LIKE :ch ",
+                            Animal.class);
+            query.setParameter("ch", new String(character + "%"));
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t find by name first letter: "
+                    + character, e);
+        }
     }
 }
