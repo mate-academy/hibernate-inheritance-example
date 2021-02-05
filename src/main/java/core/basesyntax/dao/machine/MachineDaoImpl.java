620f00ea10e7ce37d0e5ffc68afa0f034b31806b
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-05 18:24:56 +0200

    Implemented the MachineDaoImpl.java with annotations in the model classes

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,8 +2,11 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,34 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(machine);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Errored while adding " + machine);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Machine where age > :age", Machine.class)
+                    .setParameter("age", age).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Errored while getting data by "
+                                       + age + " age.");
+        }
     }
 }
--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -31,4 +31,12 @@ public class Animal {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
