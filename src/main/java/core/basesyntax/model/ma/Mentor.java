fe31f826f552ce3adf3d90d7401f7dfc062fd994
Vadim <Vedroidua@gmail.com>
2021-02-05 22:27:28 +0200

    Added the implementation of Join tables inheritance strategy.

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,16 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mentors")
 public class Mentor extends Person {
 
+    @Override
+    public String toString() {
+        return "Mentor{"
+                + super.toString()
+                + '}';
+    }
 }
