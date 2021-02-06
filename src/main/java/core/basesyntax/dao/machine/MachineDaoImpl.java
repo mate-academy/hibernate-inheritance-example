f1df62610d723df7836a852368521794ded05262
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-06 19:19:05 +0200

    Tested MachineDaoImpl

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -3,10 +3,12 @@ package core.basesyntax.dao.machine;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
 import core.basesyntax.model.zoo.Animal;
+import java.time.LocalDate;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -38,8 +40,12 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
         try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("from Machine where age > :age", Machine.class)
-                    .setParameter("age", age).getResultList();
+            int year = LocalDate.now().getYear() - age;
+            Query<Machine> query = session
+                    .createQuery("from Machine m where m.year < :year",
+                            Machine.class);
+            query.setParameter("year", year);
+            return query.getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Errored while getting data by "
                                        + age + " age.");
--- src/test/java/core/basesyntax/dao/machine/MachineDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/machine/MachineDaoImplTest.java
@@ -1,5 +1,50 @@
 package core.basesyntax.dao.machine;
 
-public class MachineDaoImplTest {
+import core.basesyntax.dao.AbstractTest;
+import core.basesyntax.model.machine.Car;
+import core.basesyntax.model.machine.Machine;
+import core.basesyntax.model.machine.Truck;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class MachineDaoImplTest extends AbstractTest {
+    private MachineDao machineDao;
+    
+    @Override
+    protected Class<?>[] entities() {
+        return new Class[] {
+                Machine.class,
+                Car.class,
+                Truck.class
+        };
+    }
+    
+    @Before
+    public void setUp() throws Exception {
+        machineDao = new MachineDaoImpl(getSessionFactory());
+    }
+    
+    @Test
+    public void save() {
+        Machine machine = new Car();
+        machine.setMaker("Me");
+        machine.setYear(100);
+        Machine actual = machineDao.save(machine);
+        Assert.assertNotNull(actual);
+        Assert.assertNotNull(actual.getId());
+    }
+    
+    @Test
+    public void selectByAge() {
+        Machine machine1 = new Car();
+        machine1.setMaker("Me");
+        machine1.setYear(100);
+        Machine machine2 = new Car();
+        machine2.setMaker("Me");
+        machine2.setYear(1);
+        machineDao.save(machine1);
+        machineDao.save(machine2);
+        Assert.assertNotNull(machineDao.findByAgeOlderThan(10).get(0));
+    }
 }
