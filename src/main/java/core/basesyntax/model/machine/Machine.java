744c6600410e0e3e3879cbb0a98b773d7e4970ea
gigbat <b.kostevich@gmail.com>
2021-02-07 16:34:27 +0200

    Fixed mistakes assigned wit checklist

--- src/main/java/core/basesyntax/model/machine/Machine.java
+++ src/main/java/core/basesyntax/model/machine/Machine.java
@@ -1,6 +1,8 @@
 package core.basesyntax.model.machine;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -9,6 +11,7 @@ import javax.persistence.InheritanceType;
 @Inheritance (strategy = InheritanceType.TABLE_PER_CLASS)
 public class Machine {
     @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
     private int year;
     private String maker;
