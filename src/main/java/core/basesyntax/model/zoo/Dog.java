6913ea9a7e0d547d582567531267caab36b93667
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-02-07 11:05:10 +0200

    added column(names)

--- src/main/java/core/basesyntax/model/zoo/Dog.java
+++ src/main/java/core/basesyntax/model/zoo/Dog.java
@@ -1,6 +1,13 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("2")
 public class Dog extends Animal {
+    @Column(name = "tail_length")
     private int tailLength;
     private String owner;
 
