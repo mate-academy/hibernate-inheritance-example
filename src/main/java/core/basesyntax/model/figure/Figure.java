25097f87693ffcf03a18ccc64de266d0ba7de885
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-08 09:57:44 +0200

    add solution

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,6 +1,5 @@
 package core.basesyntax.model.figure;
 
-import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
