526e45bbfa487562a7fa70904a216c630bb8f379
Baldinini <baldinat063@gmail.com>
2021-02-08 14:50:01 +0200

    fixed bugs

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -10,7 +10,7 @@ import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "animal")
+@DiscriminatorColumn(name = "animal_type")
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
