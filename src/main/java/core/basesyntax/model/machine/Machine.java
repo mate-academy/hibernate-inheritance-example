cd46199aa0e9515dd7f4481101b0f09cb626624b
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-08 10:13:42 +0200

    remade type of inheritance for Machine

--- src/main/java/core/basesyntax/model/machine/Machine.java
+++ src/main/java/core/basesyntax/model/machine/Machine.java
@@ -1,11 +1,14 @@
 package core.basesyntax.model.machine;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Machine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
