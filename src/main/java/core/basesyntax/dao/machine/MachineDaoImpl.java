b2ec2a08066a2cf9d6cf62714a6514f109d50a43
ilya-brinchuk <ilya.brinchuk@gmail.com>
2021-02-08 13:07:17 +0200

    added solution

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.machine;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,38 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save in data base this machine: "
+                    + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        String hql = "SELECT m FROM Machine m "
+                + "WHERE m.year < :age";
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> query = session.createQuery(hql, Machine.class);
+            query.setParameter("age", age);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find  machines by this age: '"
+                    + age, e);
+        }
     }
 }
