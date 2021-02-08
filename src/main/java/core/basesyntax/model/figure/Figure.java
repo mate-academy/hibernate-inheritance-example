d4e910ec478905d539c1e66165f31df5368c21ea
antonpanchishko <PanchishkoMayor@gmail.com>
2021-02-08 14:34:07 +0200

    2) added inheritance strategy annotation

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -4,7 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 @Entity
 public class Figure {
     @Id
