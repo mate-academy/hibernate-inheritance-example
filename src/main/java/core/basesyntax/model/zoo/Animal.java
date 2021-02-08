eb60828dccb4877cc7492a848da66a9ab863f832
antonpanchishko <PanchishkoMayor@gmail.com>
2021-02-08 14:27:36 +0200

    1) solved task

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,6 +1,15 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
     private String name;
 
@@ -18,5 +27,22 @@ public class Animal {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{"
+                + "id=" + id
+                + ", age=" + age
+                + ", name='" + name + '\''
+                + '}';
     }
 }
