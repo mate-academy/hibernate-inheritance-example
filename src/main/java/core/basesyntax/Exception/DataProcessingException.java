3b7e7c0b5c76e70d07579620a7312ede4eb222c1
oleksandr-shulha <shulgalex@gmail.com>
2021-06-12 06:43:07 +0300

    All tests work

---
+++ src/main/java/core/basesyntax/Exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.Exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
