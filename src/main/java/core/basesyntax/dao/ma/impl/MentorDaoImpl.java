5efb44b2f6afa75505fccb2e14433b7a0a76f08f
Дмитро <dima4994@gmail.com>
2021-07-13 16:21:33 +0300

    task was done

---
+++ src/main/java/core/basesyntax/dao/ma/impl/MentorDaoImpl.java
@@ -0,0 +1,26 @@
+package core.basesyntax.dao.ma.impl;
+
+import core.basesyntax.dao.ma.MentorDao;
+import core.basesyntax.model.ma.Mentor;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
+    public MentorDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public List<Mentor> findByAgeGreaterThan(int age) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Mentor> query = session.createQuery(
+                    "FROM Mentor WHERE age > :age", Mentor.class);
+            query.setParameter("age", age);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Mentors with age greater than " + age + " not found", e);
+        }
+    }
+}
---
+++ src/main/java/core/basesyntax/dao/ma/impl/PersonDaoImpl.java
@@ -0,0 +1,36 @@
+package core.basesyntax.dao.ma.impl;
+
+import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.dao.ma.PersonDao;
+import core.basesyntax.model.ma.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class PersonDaoImpl extends AbstractDao implements PersonDao {
+    public PersonDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public Person save(Person person) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(person);
+            transaction.commit();
+            return person;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.commit();
+            }
+            throw new RuntimeException("Couldn't save person to DB. " + person, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
