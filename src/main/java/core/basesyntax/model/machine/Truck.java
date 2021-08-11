7fad4d863c6c106163efeb1b92b5a02bb4cb2831
Veronika-Alekseienko <alekseienko.n@gmail.com>
2021-08-11 15:43:05 +0300

    added solution

--- src/main/java/core/basesyntax/model/machine/Truck.java
+++ src/main/java/core/basesyntax/model/machine/Truck.java
@@ -1,8 +1,22 @@
 package core.basesyntax.model.machine;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "trucks")
 public class Truck extends Machine {
     private String color;
     private double maxAllowedWeight;
+
+    public Truck() {
+    }
+
+    public Truck(int year, String maker, String color, double maxAllowedWeight) {
+        super(year, maker);
+        this.color = color;
+        this.maxAllowedWeight = maxAllowedWeight;
+    }
 
     public String getColor() {
         return color;
