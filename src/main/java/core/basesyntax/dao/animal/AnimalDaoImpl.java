9493766ecd4ad87975fd91b610c5a8ddf2c53d1e
Vitalii Martynov <VitaliiMvDeveloper@gmail.com>
2021-09-16 02:25:59 +0300

    Initial solution

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,36 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Couldn't save animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return animal;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> getAnimalQuery = session.createQuery("from Animal a "
+                    + "where lower(a.name) like :param", Animal.class);
+            getAnimalQuery.setParameter("param", "" + Character.toLowerCase(character) + '%');
+            return getAnimalQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Couldn't get animals with the first letter "
+                    + character, e);
+        }
     }
 }
