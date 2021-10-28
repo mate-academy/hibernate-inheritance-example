f60712273de5ff2760f069c2c1bd6f62f340a9aa
Serhii Orlov <orlov30@gmail.com>
2021-10-28 09:35:20 +0300

    inheritance practise.

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -1,9 +1,14 @@
 package core.basesyntax.dao.figure;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.model.figure.Circle;
 import core.basesyntax.model.figure.Figure;
+import core.basesyntax.model.figure.Triangle;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,45 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(figure);
+            transaction.commit();
+            return figure;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert figure: " + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> query = null;
+            if (clazz == Circle.class) {
+                query = session.createQuery("FROM Circle c "
+                        + "WHERE c.color = :color", clazz);
+            } else if (clazz == Triangle.class) {
+                query = session.createQuery("FROM Triangle t "
+                        + "WHERE t.color = :color", clazz);
+            }
+            if (query != null) {
+                query.setParameter("color", color);
+                return query.getResultList();
+            } else {
+                throw new RuntimeException();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find figures with color: " + color, e);
+        }
     }
 }
