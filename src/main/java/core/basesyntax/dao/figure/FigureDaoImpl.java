40146d61adb4a0a5a54d01e98b8bdd98c3021e6f
Kyrylo Dvornyk <kiril75341@gmail.com>
2021-11-02 13:44:12 +0200

    Implemented hibernate-inheritance-example task

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -1,9 +1,13 @@
 package core.basesyntax.dao.figure;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.model.figure.Circle;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,40 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(figure);
+            transaction.commit();
+            return figure;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save figure " + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        String findByColorQuery = null;
+        if (clazz == Circle.class) {
+            findByColorQuery = "FROM Circle c WHERE c.color = :color";
+        } else {
+            findByColorQuery = "FROM Triangle t WHERE t.color = :color";
+        }
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> figureQuery = session.createQuery(findByColorQuery, clazz);
+            figureQuery.setParameter("color", color);
+            return figureQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get figure by color " + color, e);
+        }
     }
 }
