6916f2bb83a07cfaa81be6e24eeae662cba01fcd
Alexandr <netiazhevskyi@gmail.com>
2021-11-02 16:47:21 +0200

    Inheritance models are implemented

--- src/main/java/core/basesyntax/model/figure/Triangle.java
+++ src/main/java/core/basesyntax/model/figure/Triangle.java
@@ -1,7 +1,18 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Triangle extends Figure {
     private double area;
+
+    public Triangle() {
+    }
+
+    public Triangle(Long id, String color, double area) {
+        super(id, color);
+        this.area = area;
+    }
 
     public double getArea() {
         return area;
