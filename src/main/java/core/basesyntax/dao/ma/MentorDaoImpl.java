69714ee381e5ea86ee030e33410be6f7c884bc5e
Roman Polulikh <polilikh.r@gmail.com>
2021-12-30 18:25:27 +0200

    Added required annotations, implemented required methods

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +13,11 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Mentor> getMentorsQuery
+                    = session.createQuery("from Mentor where age > :age", Mentor.class);
+            getMentorsQuery.setParameter("age", age);
+            return getMentorsQuery.getResultList();
+        }
     }
 }
