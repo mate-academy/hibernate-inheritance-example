2ef603106c6cc01d1aedcd07353f010af721818c
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-12-31 13:14:08 +0200

    made Figure abstract class

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public class Figure {
+public abstract class Figure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
