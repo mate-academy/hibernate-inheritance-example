246b8db86ccb9c7174d126b1bf436960e8b36d83
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-12-31 15:28:25 +0200

    BUILD SUCCESS;

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,7 +2,10 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +14,14 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Coach> query = session.createQuery("from coach "
+                    + "where experience > :years", Coach.class);
+            query.setParameter("years", years);
+            return query.getResultList();
+        } catch (HibernateException e) {
+            throw new RuntimeException("Can't find coaches with greater experience than "
+                    + years, e);
+        }
     }
 }
