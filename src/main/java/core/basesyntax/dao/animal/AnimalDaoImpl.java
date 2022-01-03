acb82325ef703a9bcd324525bbd5aaa98959f55f
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2022-01-03 18:41:23 +0200

    implemented all DAO methods

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,38 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add animal " + animal + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            character = Character.toLowerCase(character);
+            Query<Animal> getAnimalsByFirstLetter =
+                    session.createQuery("from Animal a where upper(a.name) like upper(:character)",
+                            Animal.class);
+            getAnimalsByFirstLetter.setParameter("character", character + "%");
+            return getAnimalsByFirstLetter.getResultList();
+        } catch (HibernateException e) {
+            throw new RuntimeException("Can't get list of animals by first character "
+                    + character, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,7 +2,10 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +14,14 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Coach> getCoachByAgeGreaterThan =
+                    session.createQuery("from Coach where experience > :years", Coach.class);
+            getCoachByAgeGreaterThan.setParameter("years", years);
+            return getCoachByAgeGreaterThan.getResultList();
+        } catch (HibernateException e) {
+            throw new RuntimeException("Can't get list of coaches where age greater than "
+                    + years, e);
+        }
     }
 }
