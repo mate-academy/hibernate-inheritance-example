7f4c32f0b18f0331cefa208633a7d41302356f46
Natalia Vidzivashets <nata.vidzivashets@gmail.com>
2022-01-03 21:34:41 +0200

    implemented task hibernate-inheritance

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +13,14 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Coach> findCoachesByExperience = session.createQuery("FROM Coach "
+                    + "WHERE experience > :years", Coach.class);
+            findCoachesByExperience.setParameter("years", years);
+            return findCoachesByExperience.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all coaches with experience greater than: "
+                    + years, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +13,13 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Mentor> findMentorsByAge = session.createQuery("FROM Mentor "
+                    + "WHERE age > :age", Mentor.class);
+            findMentorsByAge.setParameter("age", age);
+            return findMentorsByAge.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all mentors by age: " + age, e);
+        }
     }
 }
