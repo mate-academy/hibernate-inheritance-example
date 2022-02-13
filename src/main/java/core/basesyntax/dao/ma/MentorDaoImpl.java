3cbb3e5f22cfc13e01ee29cce0ae86669d99f40b
forsfakel <81535727+forsfakel@users.noreply.github.com>
2022-02-14 00:27:43 +0200

    Models Animal, Cat, Dog implementation of 'Single table' inheritance strategy; Models Machine, Car, Truck implementation of 'Table per class' inheritance strategy; Models Person, Coach, Mentor implementation of 'Join table' inheritance strategy; Models Figure, Circle, Triangle implementation of 'Mapped Superclass' inheritance strategy; Implement all methods in layer of DAO

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -1,8 +1,11 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
+import core.basesyntax.util.HibernateUtil;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +14,14 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Mentor> query = session.createQuery("FROM Mentor m " + "WHERE m.age > "
+                                                              + ":age",
+                    Mentor.class);
+            query.setParameter("age", age);
+            return query.list();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find  by year: " + age, e);
+        }
     }
 }
