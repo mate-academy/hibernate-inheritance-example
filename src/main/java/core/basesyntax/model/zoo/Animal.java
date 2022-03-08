73382a899204e6d817b1d9789ca9ac44305d21d1
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-03-08 18:54:54 +0200

    Add id field to Animal

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
     private String name;
 
@@ -31,5 +32,13 @@ public class Animal {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
