afd7c754c21eb2444b7c08f605bd794225b15948
AlexPravda1 <kabinkos@gmail.com>
2022-03-11 00:16:00 +0400

    - Inheritance annotations added to models - Dao level implemeneted - Prepared Query used

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,8 +1,28 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance (strategy = InheritanceType.TABLE_PER_CLASS)
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private int age;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public int getAge() {
         return age;
