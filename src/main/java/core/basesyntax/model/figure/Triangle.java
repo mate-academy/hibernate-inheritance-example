ca47184831975d717b9290393f356846a311c756
sviat-h <svyatoslav.grinik@gmail.com>
2022-03-21 16:02:17 +0200

    figure models and figure dao layer were implemented

--- src/main/java/core/basesyntax/model/figure/Triangle.java
+++ src/main/java/core/basesyntax/model/figure/Triangle.java
@@ -1,13 +1,14 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString(callSuper = true)
+@Getter
+@Setter
+@Entity
 public class Triangle extends Figure {
     private double area;
-
-    public double getArea() {
-        return area;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
-    }
 }
