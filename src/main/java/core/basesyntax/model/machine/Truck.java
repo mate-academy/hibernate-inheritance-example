43e7477ad0a9d8d4e2c6d4857f4fcdffaebec209
sviat-h <svyatoslav.grinik@gmail.com>
2022-03-21 16:15:55 +0200

    machine models and machine dao layer were implemented

--- src/main/java/core/basesyntax/model/machine/Truck.java
+++ src/main/java/core/basesyntax/model/machine/Truck.java
@@ -1,22 +1,15 @@
 package core.basesyntax.model.machine;
 
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString(callSuper = true)
+@Getter
+@Setter
+@Entity
 public class Truck extends Machine {
     private String color;
     private double maxAllowedWeight;
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double getMaxAllowedWeight() {
-        return maxAllowedWeight;
-    }
-
-    public void setMaxAllowedWeight(double maxAllowedWeight) {
-        this.maxAllowedWeight = maxAllowedWeight;
-    }
 }
