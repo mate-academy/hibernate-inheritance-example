9d134fb95d8bfb15ec59595d3e979c1b837f00c6
sviat-h <svyatoslav.grinik@gmail.com>
2022-03-21 15:31:44 +0200

    added toString methods to Car and Dog classes

--- src/main/java/core/basesyntax/model/zoo/Cat.java
+++ src/main/java/core/basesyntax/model/zoo/Cat.java
@@ -3,7 +3,9 @@ package core.basesyntax.model.zoo;
 import javax.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString(callSuper = true)
 @Getter
 @Setter
 @Entity
