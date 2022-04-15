e657e01d0e56affd26e5faf4fcc7dc14f10f4b44
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-04-15 09:00:29 +0300

    all needed classes were implemented; all tests were passed

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,16 @@
 package core.basesyntax.model.ma;
 
-public class Mentor extends Person {
+import javax.persistence.Entity;
 
+@Entity
+public class Mentor extends Person {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int experience) {
+        this.age = experience;
+    }
 }
