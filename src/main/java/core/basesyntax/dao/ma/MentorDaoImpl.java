3fcf9a52055eb1f77eed1f1a94b4352ed3e51cbe
Maksym Pokras <mxpokras@gmail.com>
2022-04-16 19:00:54 +0200

    Checkstyle violations are eliminated

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -16,7 +15,7 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public List<Mentor> findByAgeGreaterThan(int age) {
         try (Session session = sessionFactory.openSession()) {
             Query<Mentor> query = session.createQuery(
-                    "FROM Mentor m"
+                    "FROM Mentor m "
                     + "WHERE m.age > :age", Mentor.class
             );
             query.setParameter("age", age);
