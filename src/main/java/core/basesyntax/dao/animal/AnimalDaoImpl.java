8033ca7c0c5066b8473f10af084eea3266a6e17f
Angela1608 <89303267+Angela1608@users.noreply.github.com>
2022-06-05 19:37:28 +0300

    solved hibernate-inheritance task

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +14,37 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cannot save animal " + animal);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Animal a "
+                            + "WHERE a.name LIKE :uppercase "
+                            + "OR a.name LIKE :lowercase", Animal.class)
+                    .setParameter("uppercase", Character.toUpperCase(character) + "%")
+                    .setParameter("lowercase", Character.toLowerCase(character) + "%")
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get a list of animals with character " + character);
+        }
     }
 }
