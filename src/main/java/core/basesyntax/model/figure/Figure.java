e5d1cdd64f90625808741880a6ea3c0883d04fb4
VladyslavLn <kapuuustin@gmail.com>
2022-06-06 15:53:18 +0300

    Added solution :)

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,7 +1,16 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Figure {
     private String color;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     public String getColor() {
         return color;
@@ -9,5 +18,13 @@ public class Figure {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
