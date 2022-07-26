22480d95efaea6867b620dddd089e8e8c6799bec
Anastasia <anastasian817@gmail.com>
2022-07-26 21:31:54 +0300

    implemented different inheritance strategies

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,38 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> query = session.createQuery("FROM Animal a", Animal.class);
+            return query.getResultList()
+                    .stream()
+                    .filter(a -> a.getName().toLowerCase()
+                            .charAt(0) == Character.toLowerCase(character))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get animal by name, which starts with "
+                    + character, e);
+        }
     }
 }
