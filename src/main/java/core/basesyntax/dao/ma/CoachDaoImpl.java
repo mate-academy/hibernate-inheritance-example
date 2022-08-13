d776a3f807f6c38e14c88f4f61a34dbe5f20a9cb
drapail <mv.dibrov@gmail.com>
2022-08-13 18:56:31 +0300

    Coach and Figure Daos implemented

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,7 +2,15 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+
+import core.basesyntax.model.ma.Person;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -10,7 +18,35 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     }
 
     @Override
+    public Person save(Person person) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(person);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save person: " + person, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return person;
+    }
+
+    @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Coach> criteriaQuery = criteriaBuilder.createQuery(Coach.class);
+            Root<Coach> root = criteriaQuery.from(Coach.class);
+            criteriaQuery.where(criteriaBuilder.greaterThan(root.get("experience"), years));
+            return session.createQuery(criteriaQuery).getResultList();
+        }
     }
 }
