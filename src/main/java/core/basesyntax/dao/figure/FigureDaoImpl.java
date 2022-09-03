353e712886220310c116013f28cd1da80fef1cb1
Oleksii Mospan <alexey.mospan@gmail.com>
2022-09-04 02:45:26 +0300

    Added implementation of findByColor method

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,8 +3,14 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.dao.ObjectDao;
 import core.basesyntax.dao.ObjectDaoImpl;
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.figure.Figure;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
@@ -22,6 +28,16 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(clazz);
+            Root<T> root = criteriaQuery.from(clazz);
+            Predicate colorPredicate =
+                    criteriaBuilder.equal(root.get("color"), color);
+            criteriaQuery.select(root).where(colorPredicate);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get figures with color=" + color, e);
+        }
     }
 }
