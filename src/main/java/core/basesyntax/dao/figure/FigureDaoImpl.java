5f8cd8ecc5b8f0c41692808b4d431c1377053671
Vitalii Smahlenko <smaglenko@gmail.com>
2022-09-06 12:11:40 +0300

    The hibernate inheritance task is done

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,7 +3,12 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,38 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(figure);
+            transaction.commit();
+            return figure;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save figure " + figure + " to DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<T> findFigureByColorQuery = criteriaBuilder.createQuery(clazz);
+            Root<T> colorRoot = findFigureByColorQuery.from(clazz);
+            CriteriaBuilder.In<String> colorPredicate = criteriaBuilder.in(colorRoot.get("color"));
+            colorPredicate.value(color);
+            findFigureByColorQuery.where(colorPredicate);
+            return session.createQuery(findFigureByColorQuery).getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't find figure by color " + color + " from " + clazz, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/model/figure/Circle.java
+++ src/main/java/core/basesyntax/model/figure/Circle.java
@@ -1,7 +1,20 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "circles")
 public class Circle extends Figure {
     private int radius;
+
+    public Circle() {
+    }
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
 
     public int getRadius() {
         return radius;
