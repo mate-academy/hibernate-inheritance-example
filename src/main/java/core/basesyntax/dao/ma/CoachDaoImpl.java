9a72e520d5a8f54d6b3adc6a6a0b590b21a49d55
Dmytro Lemeshchenko <dimmonishe@gmail.com>
2022-09-17 17:26:16 +0300

    Initial commit

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,6 +2,11 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
@@ -11,6 +16,18 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Coach> query = criteriaBuilder.createQuery(Coach.class);
+            Root<Coach> coachRoot = query.from(Coach.class);
+            Predicate coachPredicate = criteriaBuilder
+                    .greaterThan(coachRoot.get("experience"),
+                            years);
+            query.where(coachPredicate);
+            return session.createQuery(query).getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get list of coaches which"
+                    + " experience are greater than " + years, e);
+        }
     }
 }
