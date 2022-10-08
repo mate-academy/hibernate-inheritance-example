1c44c20f7a1dc4052632eb6d4d6928c2890c25d4
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-08 20:02:31 +0300

    realize animals

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,10 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mentors")
 public class Mentor extends Person {
 
 }
