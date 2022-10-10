3cfca046ce074d9ddaae3455ebc4cd441b6496a9
Serhii Buria <sergeiburya@gmail.com>
2022-10-10 23:26:40 +0300

    added implementation methods

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -1,9 +1,16 @@
 package core.basesyntax.dao.figure;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,38 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(figure);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t save figure: " + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return figure;
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder cb = sessionFactory.getCriteriaBuilder();
+            CriteriaQuery<T> criteriaQuery = cb.createQuery(clazz);
+            Root<T> root = criteriaQuery.from(clazz);
+            Predicate predicate = cb.equal(root.get("color"), color);
+            criteriaQuery.select(root).where(predicate);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t find color: " + color
+                    + " from class: " + clazz, e);
+        }
     }
 }
