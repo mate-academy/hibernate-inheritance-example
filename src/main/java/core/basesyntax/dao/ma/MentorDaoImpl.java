68ffccd0726340bb17fdef8ecc4d59bec9782c5b
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-10-10 19:26:22 +0300

    task is completed

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,6 +2,11 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
@@ -11,6 +16,15 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Mentor> query = criteriaBuilder.createQuery(Mentor.class);
+            Root<Mentor> from = query.from(Mentor.class);
+            Predicate predicate = criteriaBuilder.greaterThan(from.get("age"), age);
+            CriteriaQuery<Mentor> where = query.where(predicate);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find mentor by age:" + age, e);
+        }
     }
 }
