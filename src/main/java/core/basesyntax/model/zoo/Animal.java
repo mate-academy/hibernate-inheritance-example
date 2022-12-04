78b605b934f938126610805d5bdf2fd26a02cb4f
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-12-04 22:07:43 +0200

    Implement Single Table

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,8 +1,29 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity(name = "animals")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name ="Animal_Type")
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public int getAge() {
         return age;
@@ -19,4 +40,5 @@ public class Animal {
     public void setName(String name) {
         this.name = name;
     }
+
 }
