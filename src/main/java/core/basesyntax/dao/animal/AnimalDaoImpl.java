3e0fc91c7adb978308c4b89c6603247a695ea7d8
Марк Годний <markgodnij@MacBook-Air-Mark.local>
2022-12-09 13:01:23 +0100

    Implemented iheritance classes

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,13 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,38 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return animal;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Animal> animalCriteriaQuery = criteriaBuilder.createQuery(Animal.class);
+            Root<Animal> root = animalCriteriaQuery.from(Animal.class);
+            Predicate namePredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("name")),
+                    Character.toLowerCase(character) + "%");
+            animalCriteriaQuery.where(namePredicate);
+            return session.createQuery(animalCriteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find animals by name first letter: " + character, e);
+        }
     }
 }
