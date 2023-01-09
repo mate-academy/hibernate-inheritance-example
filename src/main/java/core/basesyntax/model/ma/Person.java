6f7e093eb5109870f6898da853ede4cba40b655c
DooMLeoN <levberezov01@gmail.com>
2023-01-09 18:41:41 +0200

    first commit

--- src/main/java/core/basesyntax/model/ma/Person.java
+++ src/main/java/core/basesyntax/model/ma/Person.java
@@ -1,8 +1,30 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private int age;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public int getAge() {
         return age;
