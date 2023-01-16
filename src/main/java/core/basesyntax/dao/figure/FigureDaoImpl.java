fca178e94d4b4c1dd887646a01a371cec4c44d40
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-16 14:55:16 +0200

    mvn check

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,9 +3,8 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Circle;
 import core.basesyntax.model.figure.Figure;
-import java.util.List;
-
 import core.basesyntax.model.figure.Triangle;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
