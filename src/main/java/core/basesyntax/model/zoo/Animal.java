7e47c540df6b2a3b61bbb49e4401335cda647125
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2023-01-20 12:10:47 +0200

    corrected model for /zoo

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,6 +1,20 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "animal")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private int age;
     private String name;
 
