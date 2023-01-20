0ea97bdccb9fac610dbd7e17335ebfb5c196f5ce
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2023-01-20 12:40:22 +0200

    implementation dao

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +13,13 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Mentor m "
+                            + "WHERE m.age >= :age", Mentor.class)
+                    .setParameter("age", age)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get mentors with age grater than " + age, e);
+        }
     }
 }
