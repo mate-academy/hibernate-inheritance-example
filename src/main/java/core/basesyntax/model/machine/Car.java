17c52d3a6c150cd8bbddd96c437f0cfdaa2d024d
NSay0 <krvipa@gmail.com>
2023-01-23 13:03:51 +0200

    Models annotations added. All DAOs methods implemented.

--- src/main/java/core/basesyntax/model/machine/Car.java
+++ src/main/java/core/basesyntax/model/machine/Car.java
@@ -1,22 +1,13 @@
 package core.basesyntax.model.machine;
 
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Car extends Machine {
     private int horsePower;
     private String model;
-
-    public int getHorsePower() {
-        return horsePower;
-    }
-
-    public void setHorsePower(int horsePower) {
-        this.horsePower = horsePower;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
 }
