4772e949b377294e593e5fa1d96031cb0e3e9ccd
Maksym Tseniukh <117209668+MaksymTseniukh@users.noreply.github.com>
2023-01-26 20:54:06 +0200

    Added inheritance in models

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,14 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+
+@Entity(name = "mentor")
 public class Mentor extends Person {
 
+    public Mentor() {
+    }
+
+    public Mentor(int age, String name) {
+        super(age, name);
+    }
 }
