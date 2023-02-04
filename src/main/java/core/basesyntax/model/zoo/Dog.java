7e155abe32d384be6ef4260bcfb3e02f88435f1e
Danylo Borysenko <borysenkodanyl@gmail.com>
2023-02-04 22:50:51 +0200

    All models was updated

--- src/main/java/core/basesyntax/model/zoo/Dog.java
+++ src/main/java/core/basesyntax/model/zoo/Dog.java
@@ -1,5 +1,8 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Dog extends Animal {
     private int tailLength;
     private String owner;
@@ -18,5 +21,13 @@ public class Dog extends Animal {
 
     public void setOwner(String owner) {
         this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{"
+                + "tailLength=" + tailLength
+                + ", owner='" + owner + '\''
+                + '}';
     }
 }
