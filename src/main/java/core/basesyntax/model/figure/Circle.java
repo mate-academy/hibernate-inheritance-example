3da43907eb75e135a368ceffda105c9a622623c7
Inna Diachkina <innadyachkina@gmail.com>
2023-02-17 13:03:42 +0200

    my solution

--- src/main/java/core/basesyntax/model/figure/Circle.java
+++ src/main/java/core/basesyntax/model/figure/Circle.java
@@ -1,6 +1,17 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "circles")
 public class Circle extends Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int radius;
 
     public int getRadius() {
@@ -9,5 +20,13 @@ public class Circle extends Figure {
 
     public void setRadius(int radius) {
         this.radius = radius;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
