4bdf2087397b7d3e93820de329e64196b7f32581
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2023-02-20 21:24:39 +0200

    initial commit

--- src/main/java/core/basesyntax/model/ma/Coach.java
+++ src/main/java/core/basesyntax/model/ma/Coach.java
@@ -1,13 +1,23 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "coaches")
 public class Coach extends Person {
     public enum Track {
         JAVA, FE, UI, QA
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int experience;
     @Enumerated(EnumType.STRING)
     private Track track;
@@ -26,5 +36,15 @@ public class Coach extends Person {
 
     public void setTrack(Track track) {
         this.track = track;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
     }
 }
