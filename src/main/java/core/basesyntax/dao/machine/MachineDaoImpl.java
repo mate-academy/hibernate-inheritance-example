7f7c072dffa55889064e4d8dadbc6f56726de36e
bogdanpokhilko <bogdanp1701@gmail.com>
2023-03-07 17:30:40 +0200

    HW IS DONE.

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,8 +2,15 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDateTime;
 import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,41 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not save machine: " + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            int findAge = LocalDateTime.now().getYear() - age;
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Machine> criteriaQuery =
+                    criteriaBuilder.createQuery(Machine.class);
+            Root<Machine> root = criteriaQuery.from(Machine.class);
+            Predicate predicate = criteriaBuilder.lessThan(root.get("year"), findAge);
+            Predicate allConditions = criteriaBuilder.and(predicate);
+            criteriaQuery.select(root).where(allConditions);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can not find age "
+                    + "of the machine greater than: " + age, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,6 +1,17 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Animal {
+    @Id
+    @GeneratedValue
+    private Long id;
     private int age;
     private String name;
 
@@ -18,5 +29,13 @@ public class Animal {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
