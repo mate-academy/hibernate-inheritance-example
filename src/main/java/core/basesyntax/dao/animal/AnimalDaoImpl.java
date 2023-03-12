f554746ce6ddbf9fb5904c638306fc48596be160
marjanavv@gmail.com <marjanavv@gmail.com>
2023-03-12 17:47:27 +0100

    Victory

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,40 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert figure " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> query = null;
+            String c = character.toString().toLowerCase() + "%";
+            String c1 = character.toString().toUpperCase() + "%";
+            query = session.createQuery("select a from Animal as a "
+                    + "where a.name like :param or a.name like :param2", Animal.class);
+            query.setParameter("param", c);
+            query.setParameter("param2", c1);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get animals with first letter: " + character, e);
+        }
+
     }
 }
