f3b6490a0b5853ccea4bc37b655e86d9173d0854
Sergey <sfisher@ukr.net>
2023-03-17 13:14:12 -0400

    Done

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -1,8 +1,12 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
+import core.basesyntax.model.ma.Person;
+import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +15,20 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Person> query = session.createQuery(
+                    "FROM Person p WHERE p.age > :age", Person.class);
+            query.setParameter("age", age);
+            List<Mentor> mentorList = new ArrayList<>();
+            for (Person person : query.getResultList()) {
+                if (person instanceof Mentor) {
+                    mentorList.add((Mentor) person);
+                }
+            }
+            return mentorList;
+        } catch (Exception e) {
+            throw new RuntimeException("Can not get MentorList by findByAgeGreaterThan. Age: "
+                    + age, e);
+        }
     }
 }
