34c6c732c14c12dfc2d526efa91ed7a43c643622
Dmytro Koval <dimonkoval@gmail.com>
2023-04-04 02:04:12 +0300

    1

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -11,12 +14,36 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
     }
 
     @Override
-    public T save(T figure) {
-        return null;
+    public T save(T entity) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+            return entity;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a entity: " + entity, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> query = session.createQuery("FROM " + clazz.getName() + " c "
+                    + "WHERE c.color = :color");
+            query.setParameter("color", color);
+            return query.list();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed method findByColor ", e);
+        }
     }
 }
