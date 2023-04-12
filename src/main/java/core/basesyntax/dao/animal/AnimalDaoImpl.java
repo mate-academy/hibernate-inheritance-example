818dd69c4232ecfb41761838c857a2f1b3e5723e
Nikita Melnikov <bam19921303@gmail.com>
2023-04-12 21:49:09 +0300

    All tasks done

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,20 +3,49 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
+
     public AnimalDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }        
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            String searchLetter = Character.toString(character);
+            Query<Animal> query = session.createQuery("FROM Animal a "
+                    + "WHERE LOWER(a.name) LIKE LOWER(:char)", Animal.class);
+            query.setParameter("char", searchLetter + "%");
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find Animal name by character " + character, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/model/zoo/Dog.java
+++ src/main/java/core/basesyntax/model/zoo/Dog.java
@@ -1,5 +1,8 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+
+@Entity(name = "Dogs")
 public class Dog extends Animal {
     private int tailLength;
     private String owner;
