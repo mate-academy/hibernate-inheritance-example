d06ed0421786b329e7256de3b15e4d31d188d762
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-04-14 19:11:31 +0300

    Implemented Inheritance strategies

--- src/main/java/core/basesyntax/model/zoo/Dog.java
+++ src/main/java/core/basesyntax/model/zoo/Dog.java
@@ -1,8 +1,14 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Dog extends Animal {
     private int tailLength;
     private String owner;
+
+    public Dog() {
+    }
 
     public int getTailLength() {
         return tailLength;
