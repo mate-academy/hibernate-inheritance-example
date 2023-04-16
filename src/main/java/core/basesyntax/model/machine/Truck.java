b5d8c77e6e857cee115bd88e798eeb817a16594f
Arina <crowlynx4@gmail.com>
2023-04-16 22:20:57 +0300

    added required annotations in models; impl all methods in dao packages

--- src/main/java/core/basesyntax/model/machine/Truck.java
+++ src/main/java/core/basesyntax/model/machine/Truck.java
@@ -1,8 +1,27 @@
 package core.basesyntax.model.machine;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Truck extends Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private String color;
     private double maxAllowedWeight;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getColor() {
         return color;
