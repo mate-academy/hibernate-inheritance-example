c95f4dab356df1a4463d2ae1f047d1a0f600a557
Sushko Roman <formatesus@gmail.com>
2023-04-23 18:35:30 +0300

    HW

--- src/main/java/core/basesyntax/model/ma/Person.java
+++ src/main/java/core/basesyntax/model/ma/Person.java
@@ -1,6 +1,18 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity(name = "persons")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
     private String name;
 
@@ -18,5 +30,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
