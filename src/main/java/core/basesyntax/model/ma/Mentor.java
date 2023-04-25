556ed1e3ba1484a8c243d6a8a19c983b9daea5f2
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2023-04-25 16:16:36 +0300

    impl hibernate-inheritance

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,31 @@
 package core.basesyntax.model.ma;
 
-public class Mentor extends Person {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "mentors")
+public class Mentor extends Person {
+    private String name;
+    private int age;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
