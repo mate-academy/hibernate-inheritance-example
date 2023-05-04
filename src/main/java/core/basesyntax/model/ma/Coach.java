c78efdbcf153b08eaab66fde31753587c7b34048
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-05-04 19:10:45 +0200

    Inheritance solutions

--- src/main/java/core/basesyntax/model/ma/Coach.java
+++ src/main/java/core/basesyntax/model/ma/Coach.java
@@ -1,12 +1,21 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Coach extends Person {
     public enum Track {
         JAVA, FE, UI, QA
     }
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private int experience;
     @Enumerated(EnumType.STRING)
@@ -26,5 +35,13 @@ public class Coach extends Person {
 
     public void setTrack(Track track) {
         this.track = track;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,19 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Mentor extends Person {
 
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
