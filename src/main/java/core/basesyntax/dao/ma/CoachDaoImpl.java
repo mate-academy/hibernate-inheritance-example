5272bce0c023f2f5ac31ec79bf9064bad120ba20
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-05-04 12:38:40 +0300

    done

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,6 +2,8 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
@@ -11,6 +13,13 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Coach c "
+                            + "WHERE c.experience > :years", Coach.class)
+                    .setParameter("years", years)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new HibernateException("Can't get by years: " + years, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,6 +2,8 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
@@ -11,6 +13,13 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Mentor m "
+                            + "WHERE m.age > :years", Mentor.class)
+                    .setParameter("years", age)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new HibernateException("Can't get by age: " + age, e);
+        }
     }
 }
