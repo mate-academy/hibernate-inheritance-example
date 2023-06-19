9cef121bad8198cc1392250559a6b8ddce6de8d3
Melnychuk <melnychukmyhailo@gmail.com>
2023-06-19 15:27:40 +0300

    All methods were completed.

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,38 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> query = session.createQuery(
+                    "FROM Animal a WHERE a.name LIKE :upperTemplate "
+                            + "OR  a.name LIKE :lowerTemplate ", Animal.class);
+            query.setParameter("lowerTemplate", String.valueOf(character).toLowerCase() + "%");
+            query.setParameter("upperTemplate", String.valueOf(character).toUpperCase() + "%");
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find animals name start with character: "
+                    + character, e);
+        }
     }
 }
