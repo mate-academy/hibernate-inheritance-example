2fad25f991ae41194a7c5ad4c60a6ed3e5342dc0
Artem <cr7realartem@gmail.com>
2023-06-30 22:30:52 +0300

    done

--- src/main/java/core/basesyntax/model/machine/Machine.java
+++ src/main/java/core/basesyntax/model/machine/Machine.java
@@ -1,6 +1,18 @@
 package core.basesyntax.model.machine;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private int year;
     private String maker;
 
@@ -18,5 +30,13 @@ public class Machine {
 
     public void setMaker(String maker) {
         this.maker = maker;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
