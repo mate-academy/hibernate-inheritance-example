6ef180f1a2d0fe59ed71ecee2985fe96f8c0f553
Gamemod13 <igorfetov1997@gmail.com>
2023-07-10 23:02:16 +0300

    Implement

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +14,23 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            session.save(animal);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        return animal;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> query = session.createQuery("from Animal a "
+                    + "where LOWER(a.name) like :name", Animal.class);
+            query.setParameter("name", Character.toLowerCase(character) + "%");
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
