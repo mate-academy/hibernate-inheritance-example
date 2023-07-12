5069c72a2c48378c94399a46629ca164258967ad
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-07-13 01:19:23 +0300

    add solution

--- src/main/java/core/basesyntax/model/ma/Person.java
+++ src/main/java/core/basesyntax/model/ma/Person.java
@@ -1,8 +1,28 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private int age;
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public int getAge() {
         return age;
