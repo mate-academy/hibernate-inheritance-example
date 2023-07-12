995627e01cdc0e1b824f5f679b9f178c53e52f7b
viprko <pr.and.vi@gmail.com>
2023-07-12 18:17:47 +0300

    Done

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +14,39 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save animal: " + animal);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return animal;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            String lowerCasePattern = Character.toLowerCase(character) + "%";
+            String upperCasePattern = Character.toUpperCase(character) + "%";
+            return session.createQuery("from Animal where name like :lowChar or name like :upChar",
+                            Animal.class)
+                    .setParameter("lowChar", lowerCasePattern)
+                    .setParameter("upChar", upperCasePattern)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Can't find animals with name starts with \"" + character + "\"");
+        }
     }
 }
