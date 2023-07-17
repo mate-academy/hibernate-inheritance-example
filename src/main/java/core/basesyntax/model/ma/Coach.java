bc1705f599365c1fdcb4d7d164c38a331b3effc4
eugenetolkunov <yevhentolkunov@gmail.com>
2023-07-17 19:08:16 +0300

    first commit

--- src/main/java/core/basesyntax/model/ma/Coach.java
+++ src/main/java/core/basesyntax/model/ma/Coach.java
@@ -1,9 +1,18 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Coach extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     public enum Track {
         JAVA, FE, UI, QA
     }
@@ -26,5 +35,13 @@ public class Coach extends Person {
 
     public void setTrack(Track track) {
         this.track = track;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/main/java/core/basesyntax/model/zoo/Cat.java
+++ src/main/java/core/basesyntax/model/zoo/Cat.java
@@ -1,6 +1,16 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cat extends Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private int numberOfLives;
     private String color;
 
@@ -18,5 +28,15 @@ public class Cat extends Animal {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
     }
 }
