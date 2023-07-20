0e28c3bc9754515972c720098d188b9909b6127c
Kristina Shpilchyna <kriss.mva@gmail.com>
2023-07-20 12:04:22 +0200

    solution inheritance

--- src/main/java/core/basesyntax/model/figure/Triangle.java
+++ src/main/java/core/basesyntax/model/figure/Triangle.java
@@ -1,6 +1,15 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Triangle extends Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private double area;
 
     public double getArea() {
@@ -9,5 +18,13 @@ public class Triangle extends Figure {
 
     public void setArea(double area) {
         this.area = area;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
