efe37b6baaf9fb875306dc016d02db9e9af52619
Artem Kostenko <a_kostenco@yahoo.com>
2023-07-29 23:06:35 +0300

    hibernate inheritance solution first commit

--- src/main/java/core/basesyntax/model/ma/Person.java
+++ src/main/java/core/basesyntax/model/ma/Person.java
@@ -1,6 +1,20 @@
 package core.basesyntax.model.ma;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private int age;
     private String name;
 
@@ -18,5 +32,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
