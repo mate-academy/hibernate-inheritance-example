d1394145f2663b4452d0ac8804b06061981ed384
Yura Khilko <yurakhilko@gmail.com>
2023-08-02 17:13:11 +0300

    added solution.

--- src/main/java/core/basesyntax/model/zoo/Cat.java
+++ src/main/java/core/basesyntax/model/zoo/Cat.java
@@ -1,5 +1,8 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+
+@Entity(name = "animal")
 public class Cat extends Animal {
     private int numberOfLives;
     private String color;
