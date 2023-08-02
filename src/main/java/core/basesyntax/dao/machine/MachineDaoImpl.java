d844df8159fb686e0bbbbd6d8a92060a8ca4e43c
Ros <traine41@icloud.com>
2023-08-02 17:59:08 +0100

    done

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,35 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(machine);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can`t save machine to db " + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
     }
 
     @Override
-    public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+    public List<Machine> findByAgeOlderThan(int year) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> fromMachine = session.createQuery("FROM Machine m "
+                            + " WHERE m.year < :year", Machine.class)
+                    .setParameter("year", LocalDateTime.now().getYear() - year);
+            return fromMachine.getResultList();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can`t get machines by age " + year, e);
+        }
     }
 }
