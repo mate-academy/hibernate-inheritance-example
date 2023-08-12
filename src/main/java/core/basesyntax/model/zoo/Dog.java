8d4454cb4a55d29487a093feceb054705990b9b8
-admin- <viktoriiamaherko@gmail.com>
2023-08-12 15:35:54 +0300

    Solution

--- src/main/java/core/basesyntax/model/zoo/Dog.java
+++ src/main/java/core/basesyntax/model/zoo/Dog.java
@@ -1,5 +1,10 @@
 package core.basesyntax.model.zoo;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dogs")
 public class Dog extends Animal {
     private int tailLength;
     private String owner;
