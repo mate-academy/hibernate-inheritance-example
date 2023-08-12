9aac2b8a0a5f87b5215946ae42f21ee1bc236502
Roman Kuchmii <romakuchmii@gmail.com>
2023-08-13 01:33:58 +0300

    added solution

--- src/main/java/core/basesyntax/model/machine/Car.java
+++ src/main/java/core/basesyntax/model/machine/Car.java
@@ -1,6 +1,17 @@
 package core.basesyntax.model.machine;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cars")
 public class Car extends Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int horsePower;
     private String model;
 
@@ -18,5 +29,13 @@ public class Car extends Machine {
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
