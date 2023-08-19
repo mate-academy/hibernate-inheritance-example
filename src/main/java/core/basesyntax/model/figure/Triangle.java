e9cf84ae538b4b854d92bcdc30e8fd038dd9808b
Mykhailo <mykhailokuryk16@gmail.com>
2023-08-19 20:49:59 +0300

    add solution

--- src/main/java/core/basesyntax/model/figure/Triangle.java
+++ src/main/java/core/basesyntax/model/figure/Triangle.java
@@ -1,7 +1,28 @@
 package core.basesyntax.model.figure;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "triangles")
 public class Triangle extends Figure {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
     private double area;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public double getArea() {
         return area;
