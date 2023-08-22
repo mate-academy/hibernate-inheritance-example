80307c0c1a69fa9955109f73b8999c64279613e7
Denis Balako <denisarius2000s@gmail.com>
2023-08-22 11:24:37 +0300

    init commit

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -2,21 +2,54 @@ package core.basesyntax.dao.figure;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
+import core.basesyntax.model.figure.Triangle;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
+    private static final String COLOR_PARAMETER = "color";
+
     public FigureDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public T save(T figure) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(figure);
+            transaction.commit();
+            return figure;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert figure: " + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "FROM "
+                    + (clazz.equals(Triangle.class) ? "Triangle " : "Circle ")
+                    + "WHERE color = :" + COLOR_PARAMETER;
+            Query<T> getMachineByAgeQuery =
+                    session.createQuery(hql, clazz);
+            getMachineByAgeQuery.setParameter(COLOR_PARAMETER, color);
+            return getMachineByAgeQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a figure by color: " + color, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,21 +2,52 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.Year;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
+    private static final String AGE_PARAMETER = "age";
+
     public MachineDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert machine: " + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> getMachineByAgeQuery =
+                    session.createQuery("FROM Machine m "
+                            + "WHERE m.year < :" + AGE_PARAMETER, Machine.class);
+            getMachineByAgeQuery.setParameter(AGE_PARAMETER, Year.now().minusYears(age).getValue());
+            return getMachineByAgeQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a machine by age older than: " + age, e);
+        }
     }
 }
