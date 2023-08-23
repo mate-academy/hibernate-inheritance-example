319431b52fac2ae73443a3d0bb99e52fe52e7526
holsoni <soniakostashchuk@gmail,com>
2023-08-23 18:06:14 +0300

    implemented hibernate inheritance and dao for figures, persons, animals and machines

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,20 +3,51 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
+    private static final String CHARACTER_PARAMETER = "character";
+
     public AnimalDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert an animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery(
+                            "FROM Animal a "
+                                    + "WHERE UPPER(a.name) LIKE :" + CHARACTER_PARAMETER,
+                            Animal.class)
+                    .setParameter(CHARACTER_PARAMETER, character + "%")
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find animals with names starting with "
+                    + character, e);
+        }
     }
 }
