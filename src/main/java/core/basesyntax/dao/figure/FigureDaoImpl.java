b1e3809f5c3e0a08a98712947691edba88c1bdcb
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-08-25 03:05:50 +0300

    hw-hibernate-inheritance

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,20 +3,49 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
+    private static final String COLOR = "color";
+
     public FigureDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public T save(T figure) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(figure);
+            transaction.commit();
+            return figure;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a figure: " + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> typeQuery = session.createQuery("FROM " + clazz.getSimpleName()
+                    + " f WHERE f.color = :color", clazz);
+            typeQuery.setParameter(COLOR, color);
+            return typeQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a figure by color: " + color, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,21 +2,53 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
+    private static final String NOW_YEAR = "nowYear";
+    private static final String AGE = "age";
+
     public MachineDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a machine: " + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> machineQuery = session.createQuery("FROM Machine "
+                          + "WHERE :nowYear - year > :age", Machine.class);
+            machineQuery.setParameter(NOW_YEAR, LocalDateTime.now().getYear());
+            machineQuery.setParameter(AGE, age);
+            return machineQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a machine by age: " + age, e);
+        }
     }
 }
