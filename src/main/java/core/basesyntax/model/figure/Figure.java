762e2aed54bc9edd1fcf5a892798897d6f250f70
Yaroslav Liashchenko <liaroswin@gmail.com>
2023-09-01 09:23:28 +0300

    added field id and getter with setter for it to all models

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,7 +1,16 @@
 package core.basesyntax.model.figure;
 
 public class Figure {
+    private Long id;
     private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getColor() {
         return color;
--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -14,8 +14,17 @@ import jakarta.persistence.InheritanceType;
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public int getAge() {
         return age;
