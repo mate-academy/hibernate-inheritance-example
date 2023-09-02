fb2a69811c1662ae9a67d5c33182673bdb4548cf
DOM <kasinets.ulia@gmail.com>
2023-09-02 10:39:55 +0300

    Implement the requirements

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import java.util.NoSuchElementException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,37 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(figure);
+            transaction.commit();
+            return figure;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new NoSuchElementException("Can't insert to DB figure: "
+                    + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> getFigureByColorQuery =
+                    session.createQuery("FROM " + clazz.getName() + " AS figure "
+                            + "WHERE figure.color = :color", clazz);
+            getFigureByColorQuery.setParameter("color", color);
+            return getFigureByColorQuery.getResultList();
+        } catch (Exception e) {
+            throw new NoSuchElementException("Can't get all figure", e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,7 +2,10 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import java.util.NoSuchElementException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +14,14 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Mentor> getMentorByExperienceGreaterThanQuery =
+                    session.createQuery("FROM Mentor AS m "
+                            + "WHERE m.age > :age", Mentor.class);
+            getMentorByExperienceGreaterThanQuery.setParameter("age", age);
+            return getMentorByExperienceGreaterThanQuery.getResultList();
+        } catch (Exception e) {
+            throw new NoSuchElementException("Can't get all mentor greater than " + age, e);
+        }
     }
 }
