543a40c6c2b9515152aa6c4a2e4bfa22106c0c6e
marinakusniruk <Kushnirukmv@gmail.com>
2023-09-06 10:08:41 +0200

    Hometask completed

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,10 +1,27 @@
 package core.basesyntax.model.figure;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String color;
 
     public String getColor() {
         return color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setColor(String color) {
