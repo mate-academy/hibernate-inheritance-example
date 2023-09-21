91d31f91edc52f5c10bde46ee4ba2a7a756d42ee
AndriiBohun <poikka69@gmail.com>
2023-09-21 13:52:26 +0300

    hibernate-inheritance 001

--- src/main/java/core/basesyntax/model/figure/Circle.java
+++ src/main/java/core/basesyntax/model/figure/Circle.java
@@ -1,5 +1,9 @@
 package core.basesyntax.model.figure;
 
+import jakarta.persistence.Entity;
+import java.util.Objects;
+
+@Entity(name = "circle")
 public class Circle extends Figure {
     private int radius;
 
@@ -9,5 +13,22 @@ public class Circle extends Figure {
 
     public void setRadius(int radius) {
         this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Circle circle = (Circle) o;
+        return radius == circle.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
     }
 }
