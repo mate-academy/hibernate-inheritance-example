ccee7cb3f1cdbc9c7851837c2f1f0fa17e9f100b
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-09-26 22:45:58 +0300

    added solution for hibernate-inheritance

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDate;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,35 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Unable to save a machine to DB", e.getCause());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> getMachineOlderThanAgeQuery = session.createQuery("FROM Machine m "
+                    + "WHERE m.year < :year_limit", Machine.class);
+            getMachineOlderThanAgeQuery.setParameter("year_limit", LocalDate.now().getYear() - age);
+            return getMachineOlderThanAgeQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find any machine older than " + age + " years", e);
+        }
     }
 }
