291182403f76c634ce42a1e4a65b9abb2d6c7df5
yuriistorozhuk <yurii.storozhuk@gmail.com>
2023-10-04 09:23:01 +0300

    complete<all>: hibernate-inheritance-example

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,11 @@
 package core.basesyntax.model.ma;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Mentor extends Person {
 
 }
