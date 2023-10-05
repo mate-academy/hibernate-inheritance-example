6bd1ae5b612d717e84f83c33fee9fb4156b50121
Teract10s <trs.leonov@gmail.com>
2023-10-05 11:57:02 +0300

    implemented all tasks

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.animal;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
+import jakarta.persistence.Query;
 import java.util.List;
+import java.util.Objects;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,32 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(animal);
+            transaction.commit();
+        } catch (Exception e) {
+            Objects.requireNonNull(transaction).rollback();
+            throw new RuntimeException("Can't add animal to db", e);
+        } finally {
+            Objects.requireNonNull(session).close();
+        }
+        return animal;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query query = session.createQuery(
+                    "from Animal WHERE name LIKE :fl1 OR name LIKE :fl2", Animal.class)
+                    .setParameter("fl1", character.toString().toUpperCase() + "%")
+                    .setParameter("fl2", character.toString().toLowerCase() + "%");
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find an animal with first letter: " + character, e);
+        }
     }
 }
