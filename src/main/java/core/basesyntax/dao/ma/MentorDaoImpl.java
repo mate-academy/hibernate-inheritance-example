04f5df20fa63010e1a13c27768290bb5e85987a1
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2023-10-07 13:24:53 +0300

    trained with hibernate-inheritance

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
@@ -11,6 +12,14 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "FROM Mentor where age > :age";
+            List<Mentor> mentorList = session.createQuery(hql, Mentor.class)
+                    .setParameter("age", age)
+                    .getResultList();
+            return mentorList;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get a list of mentors: ", e);
+        }
     }
 }
