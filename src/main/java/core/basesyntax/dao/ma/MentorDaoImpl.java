91e4cff3d888fe1cfd682802efea1b7e6c76faff
Oleh <popovich158@gmail.com>
2023-10-14 00:59:26 +0300

    implemented classes with each strategy

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -1,7 +1,9 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
+import jakarta.persistence.Query;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
@@ -11,6 +13,10 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query query = session.createQuery("from Mentor where age > :age");
+            query.setParameter("age", age);
+            return query.getResultList();
+        }
     }
 }
