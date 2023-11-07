63773ee9bcfca02b6ef7ad3c266a404d15198446
Vlad Kucher <kuchervlad49@gmail.com>
2023-11-07 19:21:55 +0200

    done

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
@@ -11,6 +12,22 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        List<Mentor> result = null;
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            String query = "FROM Mentor m WHERE m.age > :age";
+            result = session.createQuery(query, Mentor.class)
+                    .setParameter("age", age)
+                    .list();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to find coach with greater experience ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return result;
     }
 }
