5c68df0f427576a50e04b6d17b02418e49d04d6e
Nataliia <gzodchie@gmail.com>
2023-11-27 02:01:17 +0200

    added hibernate-inheritance-example solution

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -1,7 +1,10 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
+import jakarta.persistence.criteria.CriteriaQuery;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
@@ -11,6 +14,17 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaQuery<Coach> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Coach.class);
+            criteriaQuery.from(Coach.class);
+            return session.createQuery(criteriaQuery).getResultList().stream()
+                    .filter(c -> c.getExperience() > years)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find all coaches with experience more then: "
+                    + years + " years", e);
+        }
+
     }
 }
