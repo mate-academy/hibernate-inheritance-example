b4a8a806ba9bc19660d2b738cdb26404ed871af1
Roman Globa <globaroman@gmail.com>
2023-12-07 20:42:09 +0200

    the solve task

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -1,8 +1,14 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +17,16 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Coach> criteriaQuery = criteriaBuilder.createQuery(Coach.class);
+            Root<Coach> root = criteriaQuery.from(Coach.class);
+            Predicate expGreat = criteriaBuilder.gt(root.get("experience"), years);
+            criteriaQuery.where(expGreat);
+            Query<Coach> query = session.createQuery(criteriaQuery);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a Coach by years experience: " + years, e);
+        }
     }
 }
