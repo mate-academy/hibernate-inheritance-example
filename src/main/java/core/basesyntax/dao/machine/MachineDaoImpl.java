80fa98d9d85d9483ec9490eaa9db7ec636c71783
yevhenreuta <r3utaa@gmail.com>
2023-12-08 14:39:49 +0200

    solved task inheritance

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.machine;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,37 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save machine: " + machine);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "FROM Machine WHERE year < :targetYear";
+            int currentYear = java.time.LocalDate.now().getYear();
+            int targetYear = currentYear - age;
+            Query<Machine> query = session.createQuery(hql, Machine.class);
+            query.setParameter("targetYear", targetYear);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find machine by age older then: " + age, e);
+        }
     }
 }
