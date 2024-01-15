7356c6eda5a0b6c66649dd51e70eb04f34e2176d
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2024-01-15 10:28:25 +0200

    added changes to the class Machine

--- src/main/java/core/basesyntax/model/machine/Machine.java
+++ src/main/java/core/basesyntax/model/machine/Machine.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 public class Machine {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private int year;
     private String maker;
 
@@ -24,5 +25,13 @@ public class Machine {
 
     public void setMaker(String maker) {
         this.maker = maker;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
