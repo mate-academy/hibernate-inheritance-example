d23b8b46e1111063996a5544c88f51527313fa6a
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2024-01-15 10:10:16 +0200

    added changes to the class MENTORDAOIMPL

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -1,8 +1,12 @@
 package core.basesyntax.dao.ma;
 
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.ma.Mentor;
 import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
     public MentorDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +15,15 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Mentor> query = session.createQuery(
+                    "SELECT m FROM m "
+                    + "WHERE m.age > :age", Mentor.class);
+            query.setParameter("age", age);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t find mentor by age greater than: "
+                    + age, e);
+        }
     }
 }
