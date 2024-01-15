61b72b1d9dcdb1dc0a1119812b1c4794ee84e39b
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2024-01-15 10:33:10 +0200

    added changes to the class DataProcessingException

--- src/main/java/core/basesyntax/exception/DataProcessingException.java
+++ src/main/java/core/basesyntax/exception/DataProcessingException.java
@@ -1,7 +1,7 @@
 package core.basesyntax.exception;
 
 public class DataProcessingException extends RuntimeException {
-    public DataProcessingException (String message, Throwable cause) {
+    public DataProcessingException(String message, Throwable cause) {
         super(message, cause);
     }
 }
