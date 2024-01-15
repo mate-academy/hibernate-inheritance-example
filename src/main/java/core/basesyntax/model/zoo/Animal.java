2c374ddf9fbd28a5645362b66be77302747d4cfc
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2024-01-15 08:20:07 +0200

    added changes to the class Animal

--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,6 +1,14 @@
 package core.basesyntax.model.zoo;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int age;
     private String name;
 
