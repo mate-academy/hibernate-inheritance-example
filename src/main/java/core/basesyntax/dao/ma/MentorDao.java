51f0102555f2c16617d605d17419543d1c6a2e0f
Volodymyr Shashkov <finansist33333324@gmail.com>
2024-02-12 13:35:15 +0200

    solution

--- src/main/java/core/basesyntax/dao/ma/MentorDao.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDao.java
@@ -4,5 +4,7 @@ import core.basesyntax.model.ma.Mentor;
 import java.util.List;
 
 public interface MentorDao extends PersonDao {
+    Mentor save(Mentor mentor);
+
     List<Mentor> findByAgeGreaterThan(int age);
 }
--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDate;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,37 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(machine);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("can't save machine to DB:" + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            int currentYear = LocalDate.now().getYear();
+            int productionYear = currentYear - age;
+            Query<Machine> query = session.createQuery(
+                    "FROM Machine WHERE year < :productionYear", Machine.class);
+            query.setParameter("productionYear", productionYear);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find machines by age:" + age);
+        }
     }
 }
