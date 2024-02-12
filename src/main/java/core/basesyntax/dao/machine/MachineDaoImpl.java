c3b75d829a571e5a4e8490e3be4a905031f42f32
Oleksii Bilko <lucheus@gmail.com>
2024-02-12 17:59:57 +0200

    add inheritance strategies and dao

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.machine;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,36 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't add machine :" + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> query = session.createQuery("FROM Machine "
+                    + "WHERE EXTRACT(YEAR FROM CURRENT_DATE) - year > :age",
+                    Machine.class);
+            query.setParameter("age", age);
+            return query.getResultList();
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
