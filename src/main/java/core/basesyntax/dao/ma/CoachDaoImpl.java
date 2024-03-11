dc7ebdc61200f28b3caad4f6fa8b11de3c0e1574
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2024-03-11 17:39:50 +0200

    -added required annotations in model classes -implemented all methods in DAOs

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -1,7 +1,12 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
@@ -11,6 +16,15 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Coach> query = criteriaBuilder.createQuery(Coach.class);
+            Root<Coach> root = query.from(Coach.class);
+            Predicate gt = criteriaBuilder.gt(root.get("experience"), years);
+            query.select(root).where(gt);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t get a experience by gt years: " + years, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -1,7 +1,12 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Mentor;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
@@ -11,6 +16,15 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Mentor> query = criteriaBuilder.createQuery(Mentor.class);
+            Root<Mentor> root = query.from(Mentor.class);
+            Predicate gt = criteriaBuilder.gt(root.get("age"), age);
+            query.select(root).where(gt);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t get an age by gt: " + age, e);
+        }
     }
 }
