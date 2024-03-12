b8f5b5cfc0a3bef4e9de0aea1c2aac7e0fecd96b
AdamMudrak <mudrets2000@gmail.com>
2024-03-12 13:25:23 +0200

    Add AnimalDao

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,11 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,38 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
-    }
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert an animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }    }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> query = session.createQuery("FROM Animal a "
+                    + "WHERE a.name " +
+                    "LIKE :characterToUpperCase OR a.name " +
+                    "LIKE :characterToLowerCase", Animal.class);
+            query.setParameter("characterToUpperCase",String.valueOf(character).toUpperCase() + '%');
+            query.setParameter("characterToLowerCase",String.valueOf(character).toLowerCase() + '%');
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get list of animal whose names start with: "
+                    + character, e);
+        }
     }
 }
