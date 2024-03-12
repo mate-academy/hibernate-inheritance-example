47dfeedfd93b3e04885c6d47a246639a958d8a7f
AdamMudrak <mudrets2000@gmail.com>
2024-03-12 13:48:55 +0200

    Add MachineDao

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -1,9 +1,14 @@
 package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.model.ma.Mentor;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDate;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,38 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a machine: " + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        int currentYear = LocalDate.now().getYear();
+        int yearOfProduction = currentYear - age;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> query = session.createQuery("FROM Machine m "
+                    + "WHERE m.year < :yearOfProduction", Machine.class);
+            query.setParameter("yearOfProduction", yearOfProduction);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get list of mentors whose age greater than: "
+                    + age, e);
+        }
     }
 }
