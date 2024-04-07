53e1f35270ff9a106f2fc1bf191435ab73dbb6d6
Liza <lizabalashova7@gmail.com>
2024-04-07 14:23:25 +0300

    fixed

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDate;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,39 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+            if (machine == null) {
+                throw new RuntimeException("Cant craete new person " + machine);
+            }
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Exception ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            int targetYear = LocalDate.now().getYear() - age;
+            Query<Machine> query = session.createQuery("FROM Machine m "
+                    + "WHERE m.year < :age", Machine.class);
+            query.setParameter("age", targetYear);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a machine by age: " + age, e);
+        }
     }
 }
