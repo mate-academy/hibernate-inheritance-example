709cf1e87c075ffcdaa0e1689115a5fa08a94e87
trayvor <dalv23092005@gmail.com>
2024-04-18 01:27:50 +0200

    implemented required classes

--- src/main/java/core/basesyntax/model/ma/Person.java
+++ src/main/java/core/basesyntax/model/ma/Person.java
@@ -1,6 +1,18 @@
 package core.basesyntax.model.ma;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
     private String name;
 
@@ -14,6 +26,14 @@ public class Person {
 
     public String getName() {
         return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {
