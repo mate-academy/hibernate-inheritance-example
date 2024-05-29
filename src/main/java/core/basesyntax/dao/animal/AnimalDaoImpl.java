22e9e25e767352992317d1de772cbb126cfc71c7
petro feniak <petro.fenyak@gmail.com>
2024-05-29 13:03:48 +0300

    implemented methods for save and find animal entity

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -1,9 +1,16 @@
 package core.basesyntax.dao.animal;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.zoo.Animal;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,43 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save animal: "
+                    + animal + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Animal> queryForFindByName = cb.createQuery(Animal.class);
+            Root<Animal> animalRoot = queryForFindByName.from(Animal.class);
+            Predicate namePredicate =
+                    cb.or(cb.like(cb.lower(animalRoot.get("name")),
+                                    character.toString().toLowerCase() + "%"),
+                            cb.like(cb.upper(animalRoot.get("name")),
+                                    character.toString().toUpperCase() + "%"));
+            queryForFindByName.where(namePredicate);
+            return session.createQuery(queryForFindByName).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find animal by name with first letter: "
+                    + character, e);
+        }
     }
 }
