1c1bb9b590ee58dcc61d50e11fff9f0bcd1b9d93
petro feniak <petro.fenyak@gmail.com>
2024-05-30 10:35:50 +0300

    added method logic for CoachDaoImpl class

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -1,8 +1,11 @@
 package core.basesyntax.dao.ma;
 
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +14,15 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Coach> queryForFindCoach =
+                    session.createQuery("FROM Coach c "
+                            + "WHERE c.experience >:experience", Coach.class);
+            queryForFindCoach.setParameter("experience", years);
+            return queryForFindCoach.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find coach with experience greater than: "
+                    + years, e);
+        }
     }
 }
