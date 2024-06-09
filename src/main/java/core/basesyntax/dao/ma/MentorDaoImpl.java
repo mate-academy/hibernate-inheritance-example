7818c10cbc906cc71bfda55c62b9529e906bc2e1
petro feniak <petro.fenyak@gmail.com>
2024-06-09 12:07:05 +0300

    added implementation for MentorDaoImpl

--- src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/MentorDaoImpl.java
@@ -1,7 +1,13 @@
 package core.basesyntax.dao.ma;
 
+import core.basesyntax.exception.DataProcessingException;
 import core.basesyntax.model.ma.Mentor;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
@@ -11,6 +17,16 @@ public class MentorDaoImpl extends PersonDaoImpl implements MentorDao {
 
     @Override
     public List<Mentor> findByAgeGreaterThan(int age) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Mentor> queryForFindMentor = cb.createQuery(Mentor.class);
+            Root<Mentor> mentorRoot = queryForFindMentor.from(Mentor.class);
+            Predicate mentorAgePredicate = cb.gt(mentorRoot.get("age"), age);
+            queryForFindMentor.where(mentorAgePredicate);
+            return session.createQuery(queryForFindMentor).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find mentor with age greater than: "
+                    + age, e);
+        }
     }
 }
