48a1b3464656123b85a86265b4fd882c0effb93a
Anton <panyutin15@gmail.com>
2024-07-28 17:32:09 +0300

    added solution of task

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -1,9 +1,13 @@
 package core.basesyntax.dao.figure;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.model.figure.Circle;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,40 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(figure);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+                throw new RuntimeException("Can't save figure " + figure);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return figure;
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            if (clazz.equals(Circle.class)) {
+                Query query = session.createQuery("from Circle c where c.color = :color");
+                query.setParameter("color", color);
+                return query.getResultList();
+            } else {
+                Query query = session.createQuery("from Triangle t where t.color = :color");
+                query.setParameter("color", color);
+                return query.getResultList();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find by color " + color);
+        }
     }
 }
