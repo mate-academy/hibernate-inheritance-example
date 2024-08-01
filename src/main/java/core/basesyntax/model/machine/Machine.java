28c6ef0ba7ff11dc50708776fcc3f93f025d1f42
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2024-08-01 17:54:08 +0300

    initial commit

--- src/main/java/core/basesyntax/model/machine/Machine.java
+++ src/main/java/core/basesyntax/model/machine/Machine.java
@@ -1,6 +1,17 @@
 package core.basesyntax.model.machine;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private int year;
     private String maker;
 
--- src/main/java/core/basesyntax/model/zoo/Animal.java
+++ src/main/java/core/basesyntax/model/zoo/Animal.java
@@ -1,6 +1,19 @@
 package core.basesyntax.model.zoo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity(name = "animal")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int age;
     private String name;
 
