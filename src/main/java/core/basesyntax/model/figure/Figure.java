473ad68f1a91a29ce28c8d6abd507e0e0615f3cc
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-08-16 16:46:48 +0300

    hibernate-inheritance-example solution

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,6 +1,15 @@
 package core.basesyntax.model.figure;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String color;
 
     public String getColor() {
@@ -9,5 +18,13 @@ public class Figure {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
