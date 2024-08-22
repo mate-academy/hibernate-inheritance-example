7a7f989c7c16579b919ccb3000063c7db5360c6c
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2024-08-22 11:59:54 +0300

    solved homework

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +14,36 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(animal);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Unable to add animal " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return animal;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        String prefix = character.toString().toLowerCase() + "%";
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Animal a WHERE LOWER(a.name) LIKE :prefix",
+                            Animal.class)
+                    .setParameter("prefix", prefix)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find Animal entities by first letter", e);
+        }
     }
 }
