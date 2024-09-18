6b6818e61ea1bdfe33f6ff182a098b1aae3d4e6c
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-18 22:07:28 +0300

    Implemented CoachDao method logic

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -1,7 +1,11 @@
 package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
@@ -11,6 +15,15 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Coach> query = criteriaBuilder.createQuery(Coach.class);
+            Root<Coach> root = query.from(Coach.class);
+            query.where(criteriaBuilder.greaterThan(root.get("experience"), years));
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find coach with experience greater than: "
+                    + years, e);
+        }
     }
 }
