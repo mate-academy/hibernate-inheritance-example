a36551ef6924908b9fc57cc3d325f65aeba508c4
Andrey Chertykovtsev <chertiav@gmail.com>
2024-09-22 13:22:34 +0200

    Add JPA annotations and constructors to model classes

--- src/main/java/core/basesyntax/model/figure/Circle.java
+++ src/main/java/core/basesyntax/model/figure/Circle.java
@@ -1,7 +1,20 @@
 package core.basesyntax.model.figure;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "circles")
 public class Circle extends Figure {
     private int radius;
+
+    public Circle() {
+    }
+
+    public Circle(String color, int radius) {
+        super(color);
+        this.radius = radius;
+    }
 
     public int getRadius() {
         return radius;
