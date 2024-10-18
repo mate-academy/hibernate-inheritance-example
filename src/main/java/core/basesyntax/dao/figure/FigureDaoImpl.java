b0d41185f35a2507c0d96eafd300047e0783f5fb
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2024-10-18 14:20:08 +0200

    First solution

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -1,9 +1,14 @@
 package core.basesyntax.dao.figure;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.model.figure.Circle;
 import core.basesyntax.model.figure.Figure;
+import core.basesyntax.model.figure.Triangle;
+import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,45 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(figure);
+            transaction.commit();
+            return figure;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a figure: " + figure, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        List<T> figuresList = new ArrayList<>();
+        try (Session session = sessionFactory.openSession()) {
+            if (clazz == Circle.class) {
+                figuresList = session.createQuery("FROM Circle "
+                                + "WHERE color = :color", clazz)
+                        .setParameter("color", color)
+                        .getResultList();
+            } else if (clazz == Triangle.class) {
+                figuresList = session.createQuery("FROM Triangle "
+                                + "WHERE color = :color", clazz)
+                        .setParameter("color", color)
+                        .getResultList();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find a figure by color: " + color, e);
+        }
+        return figuresList;
     }
 }
+
