959621e2b457948e11850b966138583e8c2702d9
Orest Sorokowey <osorokowey@gmail.com>
2024-11-05 16:33:32 +0200

    daos implementation, model inheritance strategies

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,35 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(figure);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            throw new RuntimeException("Failed to persist entity " + figure, e);
+        }
+
+        return figure;
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> query = session.createQuery("from " + clazz.getSimpleName()
+                    + " where color = :value", clazz);
+            query.setParameter("value", color);
+            return query.getResultList();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to select an instance of "
+                    + clazz.getSimpleName()
+                    + " by color: "
+                    + color, e);
+        }
     }
 }
