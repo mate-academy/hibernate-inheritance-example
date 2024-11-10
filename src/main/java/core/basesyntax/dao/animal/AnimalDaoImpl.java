cd74da305702d8d9a9a881534f12e5aad6ac8879
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2024-11-10 16:45:57 +0100

    feat: implemented Single Table, Table per Class, Joined Table, and Mapped Superclass inheritance strategies for Animal, Machine, Person, and Figure class hierarchies

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,9 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +14,29 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(animal);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Error adding animal: " + animal, e);
+        }
+        return animal;
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Animal a WHERE LOWER(a.name)"
+                            + " LIKE :prefix", Animal.class)
+                    .setParameter("prefix", character.toString().toLowerCase() + "%")
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Error finding animals by first letter: " + character, e);
+        }
     }
 }
