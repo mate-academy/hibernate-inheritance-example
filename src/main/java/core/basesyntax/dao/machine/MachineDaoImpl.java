9788240c9226d925bd17b63674d6a34f7dc25e45
Bartlomiej Smreczak <b.smreczak@gmail.com>
2024-11-23 12:24:24 +0100

    First solution, basic tasks solved.

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -2,8 +2,12 @@ package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.machine.Machine;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,40 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert a machine: " + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        LocalDateTime localDateTime = LocalDateTime.now();
+        int currentYear = localDateTime.getYear();
+        int searchedYear = currentYear - age;
+
+        try (Session session = sessionFactory.openSession()) {
+            Query<Machine> query = session.createQuery("FROM machines m "
+                    + "WHERE m.year < :searchedYear", Machine.class);
+            query.setParameter("searchedYear", searchedYear);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find machines older than:  "
+                    + age + " years.", e);
+        }
     }
 }
