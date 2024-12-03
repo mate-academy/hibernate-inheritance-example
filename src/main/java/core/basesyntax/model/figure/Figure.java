f7fa76271e296e540f1f5ba283eda5c816527164
kankeii <vyrvyjo@gmail.com>
2024-12-03 12:40:31 +0200

    completed homework

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,13 +1,19 @@
 package core.basesyntax.model.figure;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
 public class Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String color;
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
