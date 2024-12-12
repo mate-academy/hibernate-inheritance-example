1443b2247a6bd391089d292ad1a6123ddeb0c433
d.nosenko <daniil.nosenkoo@gmail.com>
2024-12-12 17:06:37 +0100

    Add Hibernate annotations and transactional support
    Introduced Hibernate annotations to map entities with database tables and inheritance strategies across the model classes. Implemented transactional support and query methods in DAO implementations to handle database operations, with appropriate exception handling via a custom `DataProcessingException`.

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,6 +1,19 @@
 package core.basesyntax.model.figure;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
+
+@MappedSuperclass
+@Table(name = "figures")
 public class Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "color")
     private String color;
 
     public String getColor() {
@@ -9,5 +22,13 @@ public class Figure {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
