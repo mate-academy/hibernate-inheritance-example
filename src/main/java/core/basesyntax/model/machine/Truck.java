97b3f54a78476860a2f11917db18028f6ae3bcb0
Ihor Tkachuk <iono19031@gmail.com>
2024-12-13 16:18:57 +0200

    added solution

--- src/main/java/core/basesyntax/model/machine/Truck.java
+++ src/main/java/core/basesyntax/model/machine/Truck.java
@@ -1,5 +1,10 @@
 package core.basesyntax.model.machine;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "truck")
 public class Truck extends Machine {
     private String color;
     private double maxAllowedWeight;
