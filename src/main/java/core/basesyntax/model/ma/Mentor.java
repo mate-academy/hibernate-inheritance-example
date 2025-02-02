93c3672a2a7b4cb7a613481b16db2fb8361d699f
Mich Hmk <gumeniukmich@gmail.com>
2025-02-02 18:21:56 +0200

    Inheritance strategies practiced

--- src/main/java/core/basesyntax/model/ma/Mentor.java
+++ src/main/java/core/basesyntax/model/ma/Mentor.java
@@ -1,5 +1,24 @@
 package core.basesyntax.model.ma;
 
-public class Mentor extends Person {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "mentors")
+public class Mentor extends Person {
+    /* private int experience;
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentor{" + super.toString()
+                + ", experience=" + experience;
+    } */
 }
