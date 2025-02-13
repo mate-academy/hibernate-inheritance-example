e30f7f7e93c9f701189d4753eb7e74b984f090c0
FroGitHub <andrey8102310@gmail.com>
2025-02-13 16:58:47 +0200

    add inheritance

--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,6 +3,7 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
@@ -12,11 +13,19 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            session.save(figure);
+        }
+        return figure;
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM " + clazz.getSimpleName()
+                            + " WHERE color = :color", clazz)
+                    .setParameter("color", color)
+                    .getResultList();
+        }
     }
 }
