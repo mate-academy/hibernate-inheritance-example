8b29847620f4bd4593d5c6f5e45a6a43281b8389
oKukHtina <olakukhtina95@gmail.com>
2025-02-22 18:07:10 +0200

    implemented all dao

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,26 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(animal);
+            transaction.commit();
+            return animal;
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error saving animal", e);
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> query = session.createQuery(
+                    "FROM Animal "
+                            + "WHERE LOWER(name) LIKE LOWER(:character)");
+            query.setParameter("character", character + "%");
+            return query.list();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error finding animal", e);
+        }
     }
 }
--- src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
+++ src/main/java/core/basesyntax/dao/figure/FigureDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.figure;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.figure.Figure;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class FigureDaoImpl<T extends Figure> extends AbstractDao implements FigureDao<T> {
     public FigureDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,26 @@ public class FigureDaoImpl<T extends Figure> extends AbstractDao implements Figu
 
     @Override
     public T save(T figure) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(figure);
+            transaction.commit();
+            return figure;
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error saving figure", e);
+        }
     }
 
     @Override
     public List<T> findByColor(String color, Class<T> clazz) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<T> query = session.createQuery(
+                    "FROM " + clazz.getName()
+                            + " WHERE color = :color", clazz);
+            query.setParameter("color", color);
+            return query.list();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error finding figure by color : " + color, e);
+        }
     }
 }
