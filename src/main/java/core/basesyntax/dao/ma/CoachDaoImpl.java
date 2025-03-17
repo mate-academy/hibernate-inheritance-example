3409891baa8c9aef61b14c4c8d8ae87a40014cde
Karol Gajda <karolgaj97@interia.pl>
2025-03-17 13:55:34 +0100

    first solution

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +13,15 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Coach> getCoachByExperience =
+                    session.createQuery("From Coach c "
+                            + "WHERE c.experience > :years ", Coach.class);
+            getCoachByExperience.setParameter("years", years);
+            return getCoachByExperience.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all Coach with experience greater then"
+                    + years, e);
+        }
     }
 }
