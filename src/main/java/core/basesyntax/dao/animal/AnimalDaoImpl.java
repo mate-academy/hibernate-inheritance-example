0de933b24350b60cd8f49d1b4a00646d1ec2552b
Geks <gekatonheyr@ukr.net>
2025-04-03 15:27:08 +0300

    Had worked under homework hibernate inheritance: - made corresponding relation of inheritance strategies to given entities; - implemented all required declared methods in each DAO section; - passed all the tests.

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -3,7 +3,10 @@ package core.basesyntax.dao.animal;
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,39 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(animal);
+            transaction.commit();
+            return animal;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert the animal: " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Animal> animalQuery = session.createQuery("from Animal where name like "
+                    + ":lowerNameFirstLetter or name like :upperNameFirstLetter", Animal.class);
+            animalQuery.setParameter("lowerNameFirstLetter",
+                    Character.toLowerCase(character) + "%");
+            animalQuery.setParameter("upperNameFirstLetter",
+                    Character.toUpperCase(character) + "%");
+            return animalQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find animal by first character of anme: "
+                    + character, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/model/machine/Machine.java
+++ src/main/java/core/basesyntax/model/machine/Machine.java
@@ -1,6 +1,20 @@
 package core.basesyntax.model.machine;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table (name = "machines")
 public class Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private int year;
     private String maker;
 
@@ -18,5 +32,13 @@ public class Machine {
 
     public void setMaker(String maker) {
         this.maker = maker;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
