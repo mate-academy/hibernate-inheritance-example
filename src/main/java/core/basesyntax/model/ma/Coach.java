77f928486b8d7309a6139c29ae45484533948ad5
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-04-17 00:39:56 +0300

    added different inheritance strategies

--- src/main/java/core/basesyntax/model/ma/Coach.java
+++ src/main/java/core/basesyntax/model/ma/Coach.java
@@ -1,8 +1,10 @@
 package core.basesyntax.model.ma;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 
+@Entity
 public class Coach extends Person {
     public enum Track {
         JAVA, FE, UI, QA
@@ -11,6 +13,7 @@ public class Coach extends Person {
     private int experience;
     @Enumerated(EnumType.STRING)
     private Track track;
+    private Long id;
 
     public int getExperience() {
         return experience;
@@ -26,5 +29,13 @@ public class Coach extends Person {
 
     public void setTrack(Track track) {
         this.track = track;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/main/java/core/basesyntax/model/machine/Car.java
+++ src/main/java/core/basesyntax/model/machine/Car.java
@@ -1,6 +1,18 @@
 package core.basesyntax.model.machine;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Car extends Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private int horsePower;
     private String model;
 
@@ -18,5 +30,13 @@ public class Car extends Machine {
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
