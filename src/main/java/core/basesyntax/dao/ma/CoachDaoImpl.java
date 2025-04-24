e274ba762e651d22b7d6c1e96c83c299dddb2d9e
Takeit <larsubivec@gmail.com>
2025-04-24 19:29:20 +0400

    implemented required annotations in model classes and methods DAOs

--- src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
+++ src/main/java/core/basesyntax/dao/ma/CoachDaoImpl.java
@@ -2,7 +2,9 @@ package core.basesyntax.dao.ma;
 
 import core.basesyntax.model.ma.Coach;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
     public CoachDaoImpl(SessionFactory sessionFactory) {
@@ -11,6 +13,16 @@ public class CoachDaoImpl extends PersonDaoImpl implements CoachDao {
 
     @Override
     public List<Coach> findByExperienceGreaterThan(int years) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<Coach> coachByExperienceGreaterThanQuery = session.createQuery(
+                    "FROM Coach c "
+                            + "WHERE c.experience > :years", Coach.class
+            );
+            coachByExperienceGreaterThanQuery.setParameter("years", years);
+            return coachByExperienceGreaterThanQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not find any Coach with experience greater than "
+                    + years, e);
+        }
     }
 }
--- src/main/java/core/basesyntax/model/machine/Machine.java
+++ src/main/java/core/basesyntax/model/machine/Machine.java
@@ -1,6 +1,16 @@
 package core.basesyntax.model.machine;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Machine {
+    @Id @GeneratedValue
+    private Long id;
     private int year;
     private String maker;
 
@@ -18,5 +28,13 @@ public class Machine {
 
     public void setMaker(String maker) {
         this.maker = maker;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
