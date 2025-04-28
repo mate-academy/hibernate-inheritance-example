7d9178f6ef36883dded74b3327d0be8f6fab156c
vDmytriv01 <vadymdmytriv@gmail.com>
2025-04-28 16:16:21 +0200

    feat(animal): implement Single Table inheritance strategy and AnimalDaoImpl

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -2,8 +2,13 @@ package core.basesyntax.dao.animal;
 
 import core.basesyntax.dao.AbstractDao;
 import core.basesyntax.model.zoo.Animal;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
     public AnimalDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +17,39 @@ public class AnimalDaoImpl extends AbstractDao implements AnimalDao {
 
     @Override
     public Animal save(Animal animal) {
-        return null;
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            session.save(animal);
+            tx.commit();
+            return animal;
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new RuntimeException("Can't save animal " + animal, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Animal> findByNameFirstLetter(Character character) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Animal> criteria = builder.createQuery(Animal.class);
+            Root<Animal> root = criteria.from(Animal.class);
+            criteria.select(root)
+                    .where(builder.like(
+                            builder.lower(root.get("name")),
+                            character.toString().toLowerCase() + "%"));
+            return session.createQuery(criteria).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find animal by name first letter " + character, e);
+        }
     }
 }
