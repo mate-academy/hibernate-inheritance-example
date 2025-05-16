e498b3dde88d0cd63d3c146d559a07150911e221
Bohdan <111230201+FireFalcons@users.noreply.github.com>
2025-05-16 13:10:31 +0300

    all tasks and tests were passed successfully

--- src/main/java/core/basesyntax/model/machine/Truck.java
+++ src/main/java/core/basesyntax/model/machine/Truck.java
@@ -1,8 +1,25 @@
 package core.basesyntax.model.machine;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Truck extends Machine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
     private String color;
     private double maxAllowedWeight;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getColor() {
         return color;
