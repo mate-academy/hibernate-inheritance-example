64b508d495aa386d153f8ac3b5565723645824ca
Wojtek-A <wojtek.a@hotmail.com>
2025-05-18 12:34:40 +0200

    Initial commit.

--- src/main/java/core/basesyntax/model/figure/Figure.java
+++ src/main/java/core/basesyntax/model/figure/Figure.java
@@ -1,7 +1,26 @@
 package core.basesyntax.model.figure;
 
-public class Figure {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
+
+@MappedSuperclass
+@Table(name = "figures")
+public abstract class Figure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getColor() {
         return color;
